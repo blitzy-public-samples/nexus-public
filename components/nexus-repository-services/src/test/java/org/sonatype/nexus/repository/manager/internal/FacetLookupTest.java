5decebdc13a0b025d0a6942d8986b3ce41f587d8
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-10 19:38:49 +0530

    File: components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/manager/internal/FacetLookupTest.java

--- components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/manager/internal/FacetLookupTest.java
+++ components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/manager/internal/FacetLookupTest.java
@@ -16,20 +16,23 @@ import org.sonatype.goodies.testsupport.TestSupport;
 import org.sonatype.nexus.repository.Facet;
 import org.sonatype.nexus.repository.FacetSupport;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
 public class FacetLookupTest
     extends TestSupport
 {
   private FacetLookup underTest;
 
-  @Before
+  @BeforeEach
   public void setUp() {
     underTest = new FacetLookup();
   }

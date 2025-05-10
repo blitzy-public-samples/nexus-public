92ee935f007543546b3b6e7d0f460e26122f61c4
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-10 17:30:58 +0530

    File: components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/security/rest/RepositoryPrivilegeApiResourceV1.java

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/security/rest/RepositoryPrivilegeApiResourceV1.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/security/rest/RepositoryPrivilegeApiResourceV1.java
@@ -14,10 +14,10 @@ package org.sonatype.nexus.repository.security.rest;
 
 import java.util.Map;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
-import javax.ws.rs.Path;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.Path;
 
 import org.sonatype.nexus.security.SecuritySystem;
 import org.sonatype.nexus.security.internal.rest.SecurityApiResourceV1;

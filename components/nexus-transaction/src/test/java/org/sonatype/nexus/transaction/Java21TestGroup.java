d26275c6d42cb6fc4013d14ae8faa2fdfe74f71b
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 00:40:32 +0530

    File: components/nexus-transaction/src/test/java/org/sonatype/nexus/transaction/Java21TestGroup.java

---
+++ components/nexus-transaction/src/test/java/org/sonatype/nexus/transaction/Java21TestGroup.java
@@ -0,0 +1,27 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.transaction;
+
+/**
+ * Marker interface for tests that specifically validate Java 21 features.
+ * <p>
+ * This interface is used with JUnit Jupiter's @Tag annotation to selectively run or filter
+ * tests that verify Java 21 specific functionality, enabling targeted testing of Java 21
+ * compatibility during build and CI processes.
+ *
+ * @since 3.60
+ */
+public interface Java21TestGroup
+{
+  // empty
+}

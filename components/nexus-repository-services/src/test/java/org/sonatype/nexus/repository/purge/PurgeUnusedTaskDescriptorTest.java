9c361843999e937e0a301241a5c778686214c6bd
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-10 18:28:31 +0530

    File: components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/purge/PurgeUnusedTaskDescriptorTest.java

--- components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/purge/PurgeUnusedTaskDescriptorTest.java
+++ components/nexus-repository-services/src/test/java/org/sonatype/nexus/repository/purge/PurgeUnusedTaskDescriptorTest.java
@@ -18,8 +18,8 @@ import org.sonatype.nexus.formfields.FormField;
 import org.sonatype.nexus.formfields.NumberTextFormField;
 import org.sonatype.nexus.scheduling.TaskDescriptor;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -31,7 +31,7 @@ public class PurgeUnusedTaskDescriptorTest
 {
   private TaskDescriptor purgeUnusedTaskDescriptor;
 
-  @Before
+  @BeforeEach
   public void before() {
     purgeUnusedTaskDescriptor = new PurgeUnusedTaskDescriptor();
   }
@@ -40,16 +40,17 @@ public class PurgeUnusedTaskDescriptorTest
    * Ensures the construction of the descriptor has the appropriate/default values
    */
   @Test
-  public void testDescriptorConfig() {
+  public void descriptorConfig() {
     List<FormField> formFields = purgeUnusedTaskDescriptor.getFormFields();
 
     assertThat(formFields.size(), is(2));
     assertThat(formFields.get(0).getId(), is(REPOSITORY_NAME_FIELD_ID));
 
-    NumberTextFormField lastUsedField = (NumberTextFormField) formFields.get(1);
-
-    assertThat(lastUsedField.getId(), is(PurgeUnusedTask.LAST_USED_FIELD_ID));
-    assertThat(lastUsedField.getMinimumValue(), is(LAST_USED_MIN_VALUE));
-    assertThat(lastUsedField.getInitialValue(), is(LAST_USED_INIT_VALUE));
+    // Using Java 21 pattern matching for instanceof
+    if (formFields.get(1) instanceof NumberTextFormField lastUsedField) {
+      assertThat(lastUsedField.getId(), is(PurgeUnusedTask.LAST_USED_FIELD_ID));
+      assertThat(lastUsedField.getMinimumValue(), is(LAST_USED_MIN_VALUE));
+      assertThat(lastUsedField.getInitialValue(), is(LAST_USED_INIT_VALUE));
+    }
   }
 }

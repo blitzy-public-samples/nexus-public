d607a013bfedef22751aa72ede2b98bdf13894f1
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-10 23:04:12 +0530

    File: components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java

--- components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java
+++ components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java
@@ -12,58 +12,67 @@
  */
 package org.sonatype.nexus.rest;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Fault exchange object.
+ * 
+ * Implemented as a Java 21 record for more concise and efficient error representation.
+ * The record pattern provides immutability and automatic implementations of
+ * equals(), hashCode(), and canonical constructor.
  *
  * @since 3.0
  */
 @XmlRootElement(name = "fault")
-public class FaultXO
-{
-  @JsonProperty
-  private String id;
-
-  @JsonProperty
-  private String message;
-
+public record FaultXO(
+    @JsonProperty("id")
+    String id,
+    
+    @JsonProperty("message")
+    String message
+) {
+  /**
+   * No-args constructor for deserialization support.
+   * Required by some frameworks like Jackson and JAXB.
+   */
   public FaultXO() {
-    // empty
+    this(null, null);
+  }
+  
+  /**
+   * Static factory method for creating an empty instance.
+   * 
+   * @return A new FaultXO instance with null values
+   */
+  public static FaultXO empty() {
+    return new FaultXO(null, null);
   }
 
-  public FaultXO(final String id, final String message) {
-    this.id = id;
-    this.message = message;
-  }
-
+  /**
+   * Constructor with Throwable as the message source.
+   */
   public FaultXO(final String id, final Throwable cause) {
     this(id, cause.toString());
   }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(final String id) {
-    this.id = id;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(final String message) {
-    this.message = message;
+  
+  /**
+   * Static factory method with Throwable as the message source.
+   * 
+   * @param id The fault identifier
+   * @param cause The throwable cause
+   * @return A new FaultXO instance
+   */
+  public static FaultXO fromThrowable(final String id, final Throwable cause) {
+    return new FaultXO(id, cause.toString());
   }
 
   @Override
   public String toString() {
-    return getClass().getSimpleName() + "{" +
-        "id='" + id + '\'' +
-        ", message='" + message + '\'' +
-        '}';
+    return getClass().getSimpleName() + "{"
+        + "id='" + id + '\''
+        + ", message='" + message + '\''
+        + '}';
   }
 }

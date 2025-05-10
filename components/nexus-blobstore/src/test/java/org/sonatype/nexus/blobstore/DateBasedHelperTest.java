4af4a2a34651ce6c1880e366720c33b9be3eba53
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-10 22:18:43 +0530

    File: components/nexus-blobstore/src/test/java/org/sonatype/nexus/blobstore/DateBasedHelperTest.java

--- components/nexus-blobstore/src/test/java/org/sonatype/nexus/blobstore/DateBasedHelperTest.java
+++ components/nexus-blobstore/src/test/java/org/sonatype/nexus/blobstore/DateBasedHelperTest.java
@@ -16,73 +16,72 @@ import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class DateBasedHelperTest
+class DateBasedHelperTest
 {
   public static final OffsetDateTime NOW = OffsetDateTime.parse("2024-10-25T14:30:30Z");
 
   @Test
-  public void testGeneratePrefixesMinutesLess30() {
+  void generatePrefixesForMinutesLessThan30() {
     Duration duration = Duration.ofMinutes(1);
     List<String> prefixes = DateBasedHelper.generatePrefixes(NOW.minus(duration), NOW);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25/14/29", "2024/10/25/14/30"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25/14/29", "2024/10/25/14/30");
 
     duration = Duration.ofMinutes(5);
     prefixes = DateBasedHelper.generatePrefixes(NOW.minus(duration), NOW);
-    assertThat(prefixes,
-        containsInAnyOrder("2024/10/25/14/30", "2024/10/25/14/29", "2024/10/25/14/28", "2024/10/25/14/27",
-            "2024/10/25/14/26", "2024/10/25/14/25"));
+    assertThat(prefixes).containsExactlyInAnyOrder(
+        "2024/10/25/14/30", "2024/10/25/14/29", "2024/10/25/14/28", "2024/10/25/14/27",
+        "2024/10/25/14/26", "2024/10/25/14/25");
   }
 
   @Test
-  public void testGeneratePrefixesMinutesOver30() {
+  void generatePrefixesForMinutesOver30() {
     Duration duration = Duration.ofMinutes(31);
     List<String> prefixes = DateBasedHelper.generatePrefixes(NOW.minus(duration), NOW);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25/14"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25/14");
   }
 
   @Test
-  public void testGeneratePrefixesHoursLess24() {
+  void generatePrefixesForHoursLessThan24() {
     Duration duration = Duration.ofHours(3);
     List<String> prefixes = DateBasedHelper.generatePrefixes(NOW.minus(duration), NOW);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25/14", "2024/10/25/13", "2024/10/25/11", "2024/10/25/12"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25/14", "2024/10/25/13", "2024/10/25/11", "2024/10/25/12");
   }
 
   @Test
-  public void testGeneratePrefixesHoursOver24() {
+  void generatePrefixesForHoursOver24() {
     Duration duration = Duration.ofHours(25);
     List<String> prefixes = DateBasedHelper.generatePrefixes(NOW.minus(duration), NOW);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25", "2024/10/24"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25", "2024/10/24");
   }
 
   @Test
-  public void testGeneratePrefixesHoursOne() {
+  void generatePrefixesForOneHour() {
     Duration duration = Duration.ofHours(1);
     // sometimes current hour were not generated as prefix, so define specific value
     OffsetDateTime currentTime = OffsetDateTime.parse("2024-10-25T14:32:30Z");
     List<String> prefixes = DateBasedHelper.generatePrefixes(currentTime.minus(duration), currentTime);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25/14", "2024/10/25/13"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25/14", "2024/10/25/13");
   }
 
   @Test
-  public void testGeneratePrefixesMinutesOne() {
+  void generatePrefixesForOneMinute() {
     Duration duration = Duration.ofMinutes(1);
     // sometimes current hour were not generated as prefix, so define specific value
     OffsetDateTime currentTime = OffsetDateTime.parse("2024-10-25T14:32:30Z");
     List<String> prefixes = DateBasedHelper.generatePrefixes(currentTime.minus(duration), currentTime);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25/14/32", "2024/10/25/14/31"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25/14/32", "2024/10/25/14/31");
   }
 
   @Test
-  public void testGeneratePrefixesDaysOne() {
+  void generatePrefixesForOneDay() {
     Duration duration = Duration.ofDays(1);
     // sometimes current hour were not generated as prefix, so define specific value
     OffsetDateTime currentTime = OffsetDateTime.parse("2024-10-25T14:32:30Z");
     List<String> prefixes = DateBasedHelper.generatePrefixes(currentTime.minus(duration), currentTime);
-    assertThat(prefixes, containsInAnyOrder("2024/10/25", "2024/10/24"));
+    assertThat(prefixes).containsExactlyInAnyOrder("2024/10/25", "2024/10/24");
   }
 }

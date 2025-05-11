4970889cca4138830de078e51d5b991c1a6a35af
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 01:06:28 +0530

    File: plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/tasks/RemoveSnapshotsConfig.java

--- plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/tasks/RemoveSnapshotsConfig.java
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/tasks/RemoveSnapshotsConfig.java
@@ -17,60 +17,34 @@ package org.sonatype.nexus.repository.maven.tasks;
  *
  * @since 3.0
  */
-public class RemoveSnapshotsConfig
+public record RemoveSnapshotsConfig(
+    /**
+     * The minimum number of snapshots to keep.
+     */
+    int minimumRetained,
+
+    /**
+     * Snapshots older than this will be candidates for removal.
+     */
+    int snapshotRetentionDays,
+
+    /**
+     * Whether or not to delete snapshots if a release version of the same artifact is available.
+     */
+    boolean removeIfReleased,
+
+    /**
+     * An optional period to keep snapshots around, even if a release version of the same artifact is available.
+     */
+    int gracePeriod)
 {
-  private final int minimumRetained;
-
-  private final int snapshotRetentionDays;
-
-  private final boolean removeIfReleased;
-
-  private final int gracePeriod;
-
-  public RemoveSnapshotsConfig(
-      final int minimumRetained, final int snapshotRetentionDays, final boolean removeIfReleased, final int gracePeriod)
-  {
-    this.minimumRetained = minimumRetained;
-    this.snapshotRetentionDays = snapshotRetentionDays;
-    this.removeIfReleased = removeIfReleased;
-    this.gracePeriod = gracePeriod;
-  }
-
-  /**
-   * The minimum number of snapshots to keep.
-   */
-  public int getMinimumRetained() {
-    return minimumRetained;
-  }
-
-  /**
-   * Snapshots older than this will be candidates for removal.
-   */
-  public int getSnapshotRetentionDays() {
-    return snapshotRetentionDays;
-  }
-
-  /**
-   * Whether or not to delete snapshots if a release version of the same artifact is available.
-   */
-  public boolean getRemoveIfReleased() {
-    return removeIfReleased;
-  }
-
-  /**
-   * An optional period to keep snapshots around, even if a release version of the same artifact is available.
-   */
-  public int getGracePeriod() {
-    return gracePeriod;
-  }
-
   @Override
   public String toString() {
-    return getClass().getSimpleName() + "{" +
-        "minimumRetained=" + minimumRetained +
-        ", snapshotRetentionDays=" + snapshotRetentionDays +
-        ", removeIfReleased=" + removeIfReleased +
-        ", gracePeriod=" + gracePeriod +
+    return STR."{getClass().getSimpleName()}{" +
+        "minimumRetained={minimumRetained}" +
+        ", snapshotRetentionDays={snapshotRetentionDays}" +
+        ", removeIfReleased={removeIfReleased}" +
+        ", gracePeriod={gracePeriod}" +
         "}";
   }
 }

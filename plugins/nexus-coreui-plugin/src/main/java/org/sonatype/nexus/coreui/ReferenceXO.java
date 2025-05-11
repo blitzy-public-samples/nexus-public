f01f9b42d7ef729c31b0bfbcbdc76e43af7bb0ce
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 17:05:10 +0530

    File: plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/ReferenceXO.java

--- plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/ReferenceXO.java
+++ plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/ReferenceXO.java
@@ -12,7 +12,6 @@
  */
 package org.sonatype.nexus.coreui;
 
-import java.util.Objects;
 import javax.validation.constraints.NotBlank;
 
 /**
@@ -20,61 +19,14 @@ import javax.validation.constraints.NotBlank;
  *
  * @since 3.0
  */
-public class ReferenceXO
-{
-  @NotBlank
-  private String id;
-
-  @NotBlank
-  private String name;
-
-  public ReferenceXO() {
-    // empty
-  }
-
-  public ReferenceXO(final String id, final String name) {
-    this.id = id;
-    this.name = name;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    ReferenceXO other = (ReferenceXO) o;
-    return Objects.equals(id, other.id) && Objects.equals(name, other.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  @Override
-  public String toString() {
-    return "ReferenceXO{" +
-        "id='" + id + '\'' +
-        ", name='" + name + '\'' +
-        '}';
+public record ReferenceXO(
+    @NotBlank String id,
+    @NotBlank String name
+) {
+  /**
+   * Compact constructor for validation
+   */
+  public ReferenceXO {
+    // validation happens implicitly
   }
 }

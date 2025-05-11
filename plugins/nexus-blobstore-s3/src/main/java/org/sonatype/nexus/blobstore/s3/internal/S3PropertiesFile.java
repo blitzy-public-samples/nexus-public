2cba71550219b3a670aa8bf0dc7c897fc213c192
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 08:46:04 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3PropertiesFile.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3PropertiesFile.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3PropertiesFile.java
@@ -26,12 +26,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.lang.String.format;
 import static org.sonatype.nexus.blobstore.api.BlobAttributesConstants.HEADER_PREFIX;
 import static org.sonatype.nexus.blobstore.api.BlobStore.TEMPORARY_BLOB_HEADER;
 
 /**
  * Persistent properties file stored in AWS S3.
+ * 
+ * This implementation is compatible with Java 21 and leverages modern language features
+ * such as pattern matching for instanceof and string templates. The class handles
+ * loading and storing property files in Amazon S3 storage.
+ * 
+ * This class is designed to work with Java 21 and AWS SDK for Java.
  *
  * @since 3.6.1
  */
@@ -46,14 +51,26 @@ public class S3PropertiesFile
 
   private final String key;
 
+  /**
+   * Constructs a new S3PropertiesFile.
+   * 
+   * @param s3 The AmazonS3 client
+   * @param bucket The S3 bucket name
+   * @param key The S3 object key
+   */
   public S3PropertiesFile(final AmazonS3 s3, final String bucket, final String key) {
     this.s3 = checkNotNull(s3);
     this.bucket = checkNotNull(bucket);
     this.key = checkNotNull(key);
   }
 
+  /**
+   * Loads properties from S3 object.
+   * 
+   * @throws IOException if an I/O error occurs during loading
+   */
   public void load() throws IOException {
-    log.debug("Loading: {}/{}", bucket, key);
+    log.debug(STR."Loading: \{bucket}/\{key}");
 
     try (S3Object object = s3.getObject(bucket, key)) {
       try (InputStream inputStream = object.getObjectContent()) {
@@ -62,6 +79,11 @@ public class S3PropertiesFile
     }
   }
 
+  /**
+   * Creates and returns metadata for the S3 object.
+   * 
+   * @return The object metadata
+   */
   @Override
   public ObjectMetadata getMetadata() {
     ObjectMetadata metadata = new ObjectMetadata();
@@ -69,6 +91,12 @@ public class S3PropertiesFile
     return metadata;
   }
 
+  /**
+   * Gets the data as a ByteArrayOutputStream.
+   * 
+   * @return The data stream
+   * @throws IOException if an I/O error occurs during data retrieval
+   */
   @Override
   public ByteArrayOutputStream getData() throws IOException {
     ByteArrayOutputStream bufferStream = new ByteArrayOutputStream();
@@ -76,36 +104,64 @@ public class S3PropertiesFile
     return bufferStream;
   }
 
+  /**
+   * Writes data to S3 with the provided metadata.
+   * 
+   * @param data The data to write
+   * @param metadata The metadata to associate with the S3 object
+   */
   @Override
   protected void write(final ByteArrayOutputStream data, final ObjectMetadata metadata) {
-    log.debug("Storing: {}/{}", bucket, key);
+    log.debug(STR."Storing: \{bucket}/\{key}");
     byte[] buffer = data.toByteArray();
     metadata.setContentLength(buffer.length);
     s3.putObject(bucket, key, new ByteArrayInputStream(buffer), metadata);
   }
 
+  /**
+   * Adds temporary blob metadata if applicable.
+   * 
+   * @param metadata The metadata to potentially modify
+   */
   private void maybePutTempBlobUserMetadata(final ObjectMetadata metadata) {
     if (containsKey(HEADER_PREFIX + TEMPORARY_BLOB_HEADER)) {
       metadata.addUserMetadata(TEMPORARY_BLOB_HEADER, "true");
     }
   }
 
+  /**
+   * Checks if the S3 object exists.
+   * 
+   * @return true if the object exists, false otherwise
+   * @throws IOException if an I/O error occurs during the check
+   */
   public boolean exists() throws IOException {
     return s3.doesObjectExist(bucket, key);
   }
 
+  /**
+   * Removes the S3 object.
+   * 
+   * @throws IOException if an I/O error occurs during removal
+   */
   public void remove() throws IOException {
     s3.deleteObject(bucket, key);
   }
 
   public String toString() {
-    return format("s3://%s/%s %s", bucket, key, super.toString());
+    return STR."s3://\{bucket}/\{key} \{super.toString()}";
   }
 
+  /**
+   * Compares this object with another for equality.
+   * Uses Java 21 pattern matching for instanceof to simplify the code.  
+   * 
+   * @param object The object to compare with
+   * @return true if the objects are equal, false otherwise
+   */
   @Override
   public boolean equals(Object object) {
-    if (object instanceof S3PropertiesFile) {
-      S3PropertiesFile other = (S3PropertiesFile) object;
+    if (object instanceof S3PropertiesFile other) {
       return
           s3.equals(other.s3) &&
               bucket.equals(other.bucket) &&
@@ -117,6 +173,11 @@ public class S3PropertiesFile
     }
   }
 
+  /**
+   * Generates a hash code for this object.
+   * 
+   * @return The hash code value
+   */
   @Override
   public int hashCode() {
     return

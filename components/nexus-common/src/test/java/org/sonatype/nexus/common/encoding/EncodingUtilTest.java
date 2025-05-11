3f2db8edb2d6a444aa7c77acc19a3011cf01abab
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 06:37:31 +0530

    File: components/nexus-common/src/test/java/org/sonatype/nexus/common/encoding/EncodingUtilTest.java

--- components/nexus-common/src/test/java/org/sonatype/nexus/common/encoding/EncodingUtilTest.java
+++ components/nexus-common/src/test/java/org/sonatype/nexus/common/encoding/EncodingUtilTest.java
@@ -12,28 +12,31 @@
  */
 package org.sonatype.nexus.common.encoding;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Tests for {@link EncodingUtil} class.
+ */
 public class EncodingUtilTest
 {
   @Test
-  public void testUrlEncode() {
+  void testUrlEncode() {
     assertThat(EncodingUtil.urlEncode("simple"), is("simple"));
     assertThat(EncodingUtil.urlEncode("sim/ple"), is("sim%2Fple"));
   }
 
   @Test
-  public void testUrlDecode() {
+  void testUrlDecode() {
     assertThat(EncodingUtil.urlDecode("simple"), is("simple"));
     assertThat(EncodingUtil.urlDecode("sim%2Fple"), is("sim/ple"));
   }
 
   @Test
-  public void testUrlDecodeArray() {
+  void testUrlDecodeArray() {
     assertThat(EncodingUtil.urlDecode("simple", "simple2"), arrayContaining("simple", "simple2"));
     assertThat(EncodingUtil.urlDecode("sim%2Fple", "simple2%2f"), arrayContaining("sim/ple", "simple2/"));
   }

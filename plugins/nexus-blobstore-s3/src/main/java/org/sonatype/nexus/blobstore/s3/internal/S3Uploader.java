d59368e504c8ae35e544a24b1f01b463695bee76
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 08:39:41 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3Uploader.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3Uploader.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3Uploader.java
@@ -14,16 +14,32 @@ package org.sonatype.nexus.blobstore.s3.internal;
 
 import java.io.InputStream;
 
-import com.amazonaws.services.s3.AmazonS3;
+import software.amazon.awssdk.services.s3.S3Client;
 
 /**
  * Uploads a file to S3.
+ * <p>
+ * This interface has been updated for Java 21 compatibility using AWS SDK for Java 2.x.
+ * Implementations should leverage Java 21 virtual threads for I/O operations to improve
+ * performance and scalability when handling concurrent uploads.
+ * </p>
+ * 
  * @since 3.12
+ * @see <a href="https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/home.html">AWS SDK for Java 2.x</a>
  */
 public interface S3Uploader {
 
   /**
-   * Upload a file to s3.
+   * Upload a file to S3.
+   * <p>
+   * When implemented with Java 21, this method can benefit from running in a virtual thread
+   * to improve I/O throughput without consuming platform thread resources.
+   * </p>
+   * 
+   * @param s3 the S3 client
+   * @param bucket the S3 bucket name
+   * @param key the object key in the bucket
+   * @param contents the input stream containing the file contents
    */
-  void upload(AmazonS3 s3, String bucket, String key, InputStream contents);
+  void upload(S3Client s3, String bucket, String key, InputStream contents);
 }

5257c15089e6f6893fa36fa0db0d9ed1d1f734e7
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 09:46:08 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/S3Encrypter.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/S3Encrypter.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/S3Encrypter.java
@@ -12,36 +12,45 @@
  */
 package org.sonatype.nexus.blobstore.s3.internal.encryption;
 
-import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.AbstractPutObjectRequest;
+import com.amazonaws.services.s3.model.CopyObjectRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
 
 /**
- * Interface for adding encryption to S3 requests.
- * Updated to work with AWS SDK for Java 2.x.
+ * Adds any encryption necessary to S3 requests.
+ *
+ * <p>This interface uses AWS SDK for Java 1.x which is compatible with Java 21 but
+ * is in maintenance mode (as of July 31, 2024) with end-of-support on December 31, 2025.
+ * Future implementations should consider migrating to AWS SDK for Java 2.x.</p>
  *
  * @since 3.19
  */
 public interface S3Encrypter
 {
   /**
-   * Adds encryption settings to a PutObjectRequest.
+   * Adds encryption to a multipart upload initiation request.
    *
+   * @param <T> the type of request
    * @param request the request to add encryption to
+   * @return the modified request with encryption added
    */
-  void addEncryption(PutObjectRequest.Builder request);
-
+  <T extends InitiateMultipartUploadRequest> T addEncryption(T request);
+  
   /**
-   * Adds encryption settings to a CopyObjectRequest.
+   * Adds encryption to a put object request.
    *
+   * @param <T> the type of request
    * @param request the request to add encryption to
+   * @return the modified request with encryption added
    */
-  void addEncryption(CopyObjectRequest.Builder request);
-
+  <T extends AbstractPutObjectRequest> T addEncryption(T request);
+  
   /**
-   * Adds encryption settings to a CreateMultipartUploadRequest.
+   * Adds encryption to a copy object request.
    *
+   * @param <T> the type of request
    * @param request the request to add encryption to
+   * @return the modified request with encryption added
    */
-  void addEncryption(CreateMultipartUploadRequest.Builder request);
+  <T extends CopyObjectRequest> T addEncryption(T request);
 }

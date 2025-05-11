5716ff6a59d0c38d75c519af80cb6bc7ad9f9dc1
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 08:17:49 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/NoEncrypter.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/NoEncrypter.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/encryption/NoEncrypter.java
@@ -12,38 +12,35 @@
  */
 package org.sonatype.nexus.blobstore.s3.internal.encryption;
 
-import javax.inject.Named;
-
-import com.amazonaws.services.s3.model.AbstractPutObjectRequest;
-import com.amazonaws.services.s3.model.CopyObjectRequest;
-import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 /**
- * Adds no encryption to a S3 request.
+ * An {@link S3Encrypter} that does not add any encryption to requests.
+ * Updated to work with AWS SDK for Java 2.x.
  *
  * @since 3.19
  */
-@Named(NoEncrypter.ID)
 public class NoEncrypter
     implements S3Encrypter
 {
   public static final String ID = "none";
-  public static final String NAME = "None";
 
   public static final NoEncrypter INSTANCE = new NoEncrypter();
 
   @Override
-  public <T extends InitiateMultipartUploadRequest> T addEncryption(final T request) {
-    return request;
+  public void addEncryption(final PutObjectRequest.Builder request) {
+    // No encryption to add
   }
 
   @Override
-  public <T extends AbstractPutObjectRequest> T addEncryption(final T request) {
-    return request;
+  public void addEncryption(final CopyObjectRequest.Builder request) {
+    // No encryption to add
   }
 
   @Override
-  public <T extends CopyObjectRequest> T addEncryption(final T request) {
-    return request;
+  public void addEncryption(final CreateMultipartUploadRequest.Builder request) {
+    // No encryption to add
   }
 }

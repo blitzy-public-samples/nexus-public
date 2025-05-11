58e16bbde31f3a873978ffaf3fbfb843a0428f78
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 00:54:13 +0530

    File: plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/ContentDisposition.java

--- plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/ContentDisposition.java
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/ContentDisposition.java
@@ -12,13 +12,33 @@
  */
 package org.sonatype.nexus.repository.maven;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
+ * Enum representing the Content-Disposition header values for HTTP responses.
+ * Used to control how content is presented in the browser (displayed inline or as an attachment).
+ *
  * @since 3.33
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition">Content-Disposition HTTP Header</a>
  */
 public enum ContentDisposition
 {
+  /**
+   * Indicates the content should be displayed inline in the browser.
+   */
   INLINE("inline"),
+  
+  /**
+   * Indicates the content should be downloaded as an attachment.
+   */
   ATTACHMENT("attachment");
+
+  private static final Map<String, ContentDisposition> VALUE_MAP = Arrays.stream(values())
+      .collect(Collectors.toUnmodifiableMap(ContentDisposition::getValue, Function.identity()));
 
   private final String value;
 
@@ -26,7 +46,44 @@ public enum ContentDisposition
     this.value = value;
   }
 
+  /**
+   * Returns the string value of this content disposition.
+   *
+   * @return the string value
+   */
   public String getValue() {
     return value;
+  }
+  
+  /**
+   * Returns the string representation of this content disposition.
+   *
+   * @return the string representation
+   */
+  @Override
+  public String toString() {
+    return value;
+  }
+  
+  /**
+   * Converts a string value to the corresponding ContentDisposition enum.
+   *
+   * @param value the string value to convert
+   * @return the corresponding ContentDisposition, or empty if not found
+   */
+  public static Optional<ContentDisposition> fromValue(final String value) {
+    return Optional.ofNullable(value).map(VALUE_MAP::get);
+  }
+  
+  /**
+   * Converts a string value to the corresponding ContentDisposition enum.
+   * If the value doesn't match any enum, returns the default value.
+   *
+   * @param value the string value to convert
+   * @param defaultDisposition the default disposition to return if value is not found
+   * @return the corresponding ContentDisposition, or the default if not found
+   */
+  public static ContentDisposition fromValue(final String value, final ContentDisposition defaultDisposition) {
+    return fromValue(value).orElse(defaultDisposition);
   }
 }

00ac4a11eaeada98ec5bf5d0cf9923f5c05768c6
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 22:14:55 +0530

    File: plugins/nexus-repository-httpbridge/src/main/java/org/sonatype/nexus/repository/httpbridge/internal/describe/DescriptionItem.java

--- plugins/nexus-repository-httpbridge/src/main/java/org/sonatype/nexus/repository/httpbridge/internal/describe/DescriptionItem.java
+++ plugins/nexus-repository-httpbridge/src/main/java/org/sonatype/nexus/repository/httpbridge/internal/describe/DescriptionItem.java
@@ -17,35 +17,17 @@ package org.sonatype.nexus.repository.httpbridge.internal.describe;
  *
  * @since 3.0
  */
-public class DescriptionItem
-{
-  private final String name;
-
-  private final String type;
-
-  /**
-   * Value should be a primitive/char-sequence or collection/map of primitive/char-sequence
-   * to support html + json rendering w/o overly complex mapping configurations.
-   *
-   * @see DescriptionHelper#convert(Object)
-   */
-  private final Object value;
-
-  DescriptionItem(final String name, final String type, final Object value) {
-    this.name = name;
-    this.type = type;
-    this.value = value;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public Object getValue() {
-    return value;
-  }
+public record DescriptionItem(
+    String name,
+    String type,
+    /**
+     * Value should be a primitive/char-sequence or collection/map of primitive/char-sequence
+     * to support html + json rendering w/o overly complex mapping configurations.
+     *
+     * @see DescriptionHelper#convert(Object)
+     */
+    Object value
+) {
+  // Using Java 21 Record feature for immutable data holder
+  // This automatically provides constructor, getters, equals, hashCode, and toString methods
 }

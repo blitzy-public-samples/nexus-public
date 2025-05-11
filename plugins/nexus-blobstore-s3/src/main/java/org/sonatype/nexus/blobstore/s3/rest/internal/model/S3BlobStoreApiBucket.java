e638bc932e7596bdddb5ec85c41a753fd8f67196
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 11:06:46 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/rest/internal/model/S3BlobStoreApiBucket.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/rest/internal/model/S3BlobStoreApiBucket.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/rest/internal/model/S3BlobStoreApiBucket.java
@@ -25,58 +25,33 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 /**
  * Encapsulates the S3 bucket details for an s3 blob store.
+ * Implemented as a Java Record for immutability and concise representation in Java 21.
  *
  * @since 3.20
  */
 @JsonInclude(NON_NULL)
-public class S3BlobStoreApiBucket
-{
-  @Valid
-  @NotNull
-  @ApiModelProperty(value = "The AWS region to create a new S3 bucket in or an existing S3 bucket's region",
-      example = DEFAULT, required = true)
-  private final String region;
+public record S3BlobStoreApiBucket(
+    @Valid
+    @NotNull
+    @ApiModelProperty(value = "The AWS region to create a new S3 bucket in or an existing S3 bucket's region",
+        example = DEFAULT, required = true)
+    @JsonProperty("region")
+    String region,
 
-  @Valid
-  @NotNull
-  @ApiModelProperty(value = "The name of the S3 bucket", required = true)
-  private final String name;
+    @Valid
+    @NotNull
+    @ApiModelProperty(value = "The name of the S3 bucket", required = true)
+    @JsonProperty("name")
+    String name,
 
-  @ApiModelProperty("The S3 blob store (i.e S3 object) key prefix")
-  private final String prefix;
+    @ApiModelProperty("The S3 blob store (i.e S3 object) key prefix")
+    @JsonProperty("prefix")
+    String prefix,
 
-  @Valid
-  @NotNull
-  @ApiModelProperty(value = "How many days until deleted blobs are finally removed from the S3 bucket (-1 to disable)",
-      example = "3")
-  private final Integer expiration;
-
-  @JsonCreator
-  public S3BlobStoreApiBucket(
-      @JsonProperty("region") final String region,
-      @JsonProperty("name") final String name,
-      @JsonProperty("prefix") final String prefix,
-      @JsonProperty("expiration") final Integer expiration)
-  {
-    this.region = region;
-    this.name = name;
-    this.prefix = prefix;
-    this.expiration = expiration;
-  }
-
-  public String getRegion() {
-    return region;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getPrefix() {
-    return prefix;
-  }
-
-  public Integer getExpiration() {
-    return expiration;
-  }
-}
+    @Valid
+    @NotNull
+    @ApiModelProperty(value = "How many days until deleted blobs are finally removed from the S3 bucket (-1 to disable)",
+        example = "3")
+    @JsonProperty("expiration")
+    Integer expiration
+) {}

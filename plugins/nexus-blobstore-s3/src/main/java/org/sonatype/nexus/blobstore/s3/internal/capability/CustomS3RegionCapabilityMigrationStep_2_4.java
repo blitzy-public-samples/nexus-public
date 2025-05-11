d8cc67d582231aadb6a8196e44d40b80845bfc7b
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 09:53:56 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapabilityMigrationStep_2_4.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapabilityMigrationStep_2_4.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapabilityMigrationStep_2_4.java
@@ -9,16 +9,26 @@
  * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ *
+ * This class is compatible with Java 21 and OSGi/Karaf 4.4.4.
  */
 package org.sonatype.nexus.blobstore.s3.internal.capability;
 
 import java.sql.Connection;
 import java.util.Optional;
+
+// Using javax.inject which is still valid in Java 21
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.upgrade.datastore.DatabaseMigrationStep;
 
+/**
+ * Migration step that makes the CustomS3RegionCapability available.
+ * 
+ * @since 3.38
+ * @see CustomS3RegionCapability
+ */
 @Named
 @Singleton
 public class CustomS3RegionCapabilityMigrationStep_2_4
@@ -32,5 +42,6 @@ public class CustomS3RegionCapabilityMigrationStep_2_4
   @Override
   public void migrate(final Connection connection) throws Exception {
     // No-op, this makes the CustomS3RegionCapability available
+    // This method could potentially benefit from Virtual Threads in Java 21 if it performed I/O operations
   }
 }

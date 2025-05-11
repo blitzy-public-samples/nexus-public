d5750366be3ceef4c0f800739b2727ef6043c131
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 22:48:47 +0530

    File: plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/content/maven/store/Maven2ComponentData.java

--- plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/content/maven/store/Maven2ComponentData.java
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/content/maven/store/Maven2ComponentData.java
@@ -13,9 +13,35 @@
 
 package org.sonatype.nexus.content.maven.store;
 
+import java.util.Objects;
+
 import org.sonatype.nexus.repository.content.store.ComponentData;
 
 /**
+ * Maven 2 specific component data.
+ * 
+ * This class extends the base ComponentData to add Maven 2 specific attributes.
+ * It has been updated for Java 21 compatibility and follows modern Java practices.
+ * 
+ * <p>When working with this class in Java 21, you can leverage pattern matching for instanceof
+ * and record patterns for more concise code. For example:</p>
+ * 
+ * <pre>
+ * {@code
+ * // Pattern matching with instanceof
+ * if (component instanceof Maven2ComponentData maven2Component) {
+ *     String baseVersion = maven2Component.getBaseVersion();
+ *     // Use baseVersion directly
+ * }
+ * 
+ * // Using with enhanced switch expressions
+ * String result = switch(component) {
+ *     case Maven2ComponentData maven2Component -> maven2Component.getBaseVersion();
+ *     default -> component.version();
+ * };
+ * }
+ * </pre>
+ *
  * @since 3.29
  */
 public class Maven2ComponentData
@@ -23,11 +49,66 @@ public class Maven2ComponentData
 {
   private String baseVersion;
 
+  /**
+   * Returns the base version of the Maven component.
+   * 
+   * For release versions, this is the same as the version.
+   * For snapshot versions, this is the version without the timestamp.
+   *
+   * @return the base version string
+   */
   public String getBaseVersion() {
     return baseVersion;
   }
 
+  /**
+   * Sets the base version of the Maven component.
+   *
+   * @param baseVersion the base version to set
+   */
   public void setBaseVersion(final String baseVersion) {
     this.baseVersion = baseVersion;
+  }
+  
+  /**
+   * Compares this Maven2ComponentData with another object for equality.
+   * 
+   * @param o the object to compare with
+   * @return true if the objects are equal, false otherwise
+   */
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Maven2ComponentData that)) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    return Objects.equals(baseVersion, that.baseVersion);
+  }
+
+  /**
+   * Generates a hash code for this Maven2ComponentData.
+   * 
+   * @return the hash code
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), baseVersion);
+  }
+
+  /**
+   * Returns a string representation of this Maven2ComponentData.
+   * 
+   * @return a string representation
+   */
+  @Override
+  public String toString() {
+    return """
+        Maven2ComponentData{baseVersion='%s', %s}
+        """.formatted(baseVersion, super.toString());
   }
 }

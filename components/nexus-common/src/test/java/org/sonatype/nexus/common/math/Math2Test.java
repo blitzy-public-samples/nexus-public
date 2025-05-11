0c6009e7b4aaf7dcf44760ee99e9f10be918ab5d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-11 06:01:07 +0530

    File: components/nexus-common/src/test/java/org/sonatype/nexus/common/math/Math2Test.java

--- components/nexus-common/src/test/java/org/sonatype/nexus/common/math/Math2Test.java
+++ components/nexus-common/src/test/java/org/sonatype/nexus/common/math/Math2Test.java
@@ -14,7 +14,7 @@ package org.sonatype.nexus.common.math;
 
 import org.sonatype.goodies.testsupport.TestSupport;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;

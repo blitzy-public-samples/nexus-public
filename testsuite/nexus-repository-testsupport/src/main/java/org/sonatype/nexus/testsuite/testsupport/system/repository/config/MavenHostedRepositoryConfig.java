f252dfdd47eade97b8345fb275e481b4823634ed
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 22:52:58 +0530

    File: testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/MavenHostedRepositoryConfig.java

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/MavenHostedRepositoryConfig.java
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/MavenHostedRepositoryConfig.java
@@ -20,6 +20,14 @@ import org.sonatype.nexus.repository.maven.VersionPolicy;
 
 import static org.sonatype.nexus.testsuite.testsupport.system.RepositoryTestSystem.FORMAT_MAVEN;
 
+/**
+ * Configuration class for Maven hosted repositories in test environments.
+ * <p>
+ * This class is compatible with Java 21 and supports the latest test frameworks
+ * (JUnit Jupiter 5.10.1 and Mockito 5.8.0) when used in test scenarios.
+ *
+ * @since 3.0
+ */
 public class MavenHostedRepositoryConfig
     extends HostedRepositoryConfigSupport<MavenHostedRepositoryConfig>
 {
@@ -27,6 +35,11 @@ public class MavenHostedRepositoryConfig
 
   private LayoutPolicy layoutPolicy = LayoutPolicy.STRICT;
 
+  /**
+   * Constructs a new Maven hosted repository configuration with the specified factory.
+   *
+   * @param factory Function that creates a Repository from this configuration
+   */
   public MavenHostedRepositoryConfig(final Function<MavenHostedRepositoryConfig, Repository> factory) {
     super(factory);
   }
@@ -36,20 +49,42 @@ public class MavenHostedRepositoryConfig
     return FORMAT_MAVEN;
   }
 
+  /**
+   * Sets the version policy for this Maven repository.
+   *
+   * @param versionPolicy the version policy to use
+   * @return this configuration instance for method chaining
+   */
   public MavenHostedRepositoryConfig withVersionPolicy(final VersionPolicy versionPolicy) {
     this.versionPolicy = versionPolicy;
     return this;
   }
 
+  /**
+   * Gets the configured version policy.
+   *
+   * @return the version policy
+   */
   public VersionPolicy getVersionPolicy() {
     return versionPolicy;
   }
 
+  /**
+   * Sets the layout policy for this Maven repository.
+   *
+   * @param layoutPolicy the layout policy to use
+   * @return this configuration instance for method chaining
+   */
   public MavenHostedRepositoryConfig withLayoutPolicy(final LayoutPolicy layoutPolicy) {
     this.layoutPolicy = layoutPolicy;
     return this;
   }
 
+  /**
+   * Gets the configured layout policy.
+   *
+   * @return the layout policy
+   */
   public LayoutPolicy getLayoutPolicy() {
     return layoutPolicy;
   }

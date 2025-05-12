13719362853ecdf6fa7d22a15ad20359c6a58317
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 13:46:48 +0530

    File: plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/internal/rest/CertificateApiResourceDoc.java

--- plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/internal/rest/CertificateApiResourceDoc.java
+++ plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/internal/rest/CertificateApiResourceDoc.java
@@ -14,20 +14,22 @@ package com.sonatype.nexus.ssl.plugin.internal.rest;
 
 import java.util.List;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
 
 import org.sonatype.nexus.ssl.ApiCertificate;
 import com.sonatype.nexus.ssl.plugin.validator.HostnameOrIpAddress;
 import com.sonatype.nexus.ssl.plugin.validator.PemCertificate;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_CONFLICT;
@@ -37,37 +39,37 @@ import static org.apache.http.HttpStatus.SC_FORBIDDEN;
 /**
  * @since 3.19
  */
-@Api(value = "Security: certificates")
+@Tag(name = "Security: certificates")
 public interface CertificateApiResourceDoc
 {
-  @ApiOperation("Helper method to retrieve certificate details from a remote system.")
+  @Operation(summary = "Helper method to retrieve certificate details from a remote system.")
   @ApiResponses(value = {
-      @ApiResponse(code = SC_FORBIDDEN, message = "Insufficient permissions to retrieve remote certificate."),
-      @ApiResponse(code = SC_BAD_REQUEST,
-          message = "A certificate could not be retrieved, see the message for details.")})
+      @ApiResponse(responseCode = "403", description = "Insufficient permissions to retrieve remote certificate."),
+      @ApiResponse(responseCode = "400",
+          description = "A certificate could not be retrieved, see the message for details.")})
   ApiCertificate retrieveCertificate(
-      @ApiParam(value = "The remote system's host name") @NotNull @NotEmpty @HostnameOrIpAddress String host,
-      @ApiParam(value = "The port on the remote system to connect to") Integer port,
-      @ApiParam(value = "An optional hint of the protocol to try for the connection") String protocolHint);
+      @Parameter(description = "The remote system's host name") @NotNull @NotEmpty @HostnameOrIpAddress String host,
+      @Parameter(description = "The port on the remote system to connect to") Integer port,
+      @Parameter(description = "An optional hint of the protocol to try for the connection") String protocolHint);
 
-  @ApiOperation("Retrieve a list of certificates added to the trust store.")
+  @Operation(summary = "Retrieve a list of certificates added to the trust store.")
   @ApiResponses(value = {
-      @ApiResponse(code = SC_FORBIDDEN, message = "Insufficient permissions to list certificates in the trust store.")})
+      @ApiResponse(responseCode = "403", description = "Insufficient permissions to list certificates in the trust store.")})
   List<ApiCertificate> getTrustStoreCertificates();
 
-  @ApiOperation("Add a certificate to the trust store.")
+  @Operation(summary = "Add a certificate to the trust store.")
   @ApiResponses(value = {
-      @ApiResponse(code = SC_CREATED, message = "The certificate was successfully added.",
-          response = ApiCertificate.class),
-      @ApiResponse(code = SC_CONFLICT,
-          message = "The certificate already exists in the system."),
-      @ApiResponse(code = SC_FORBIDDEN, message = "Insufficient permissions to add certificate to the trust store.")})
+      @ApiResponse(responseCode = "201", description = "The certificate was successfully added.",
+          content = @Content(schema = @Schema(implementation = ApiCertificate.class))),
+      @ApiResponse(responseCode = "409",
+          description = "The certificate already exists in the system."),
+      @ApiResponse(responseCode = "403", description = "Insufficient permissions to add certificate to the trust store.")})
   Response addCertificate(
-      @ApiParam("The certificate to add encoded in PEM format") @NotBlank @PemCertificate String pem);
+      @Parameter(description = "The certificate to add encoded in PEM format") @NotBlank @PemCertificate String pem);
 
-  @ApiOperation("Remove a certificate in the trust store.")
+  @Operation(summary = "Remove a certificate in the trust store.")
   @ApiResponses(value = {
-      @ApiResponse(code = SC_FORBIDDEN,
-          message = "Insufficient permissions to remove certificate from the trust store")})
-  void removeCertificate(@ApiParam(value = "The id of the certificate that should be removed.") String id);
+      @ApiResponse(responseCode = "403",
+          description = "Insufficient permissions to remove certificate from the trust store")})
+  void removeCertificate(@Parameter(description = "The id of the certificate that should be removed.") String id);
 }

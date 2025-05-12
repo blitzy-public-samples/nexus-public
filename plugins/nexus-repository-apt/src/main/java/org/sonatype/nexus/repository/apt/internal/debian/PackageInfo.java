62a23bdf2e7113d75198408b80d4f1bcb7b86867
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 09:14:56 +0530

    File: plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/debian/PackageInfo.java

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/debian/PackageInfo.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/debian/PackageInfo.java
@@ -12,8 +12,14 @@
  */
 package org.sonatype.nexus.repository.apt.internal.debian;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 /**
+ * Represents package information extracted from a Debian control file.
+ * 
  * @since 3.17
+ * @see ControlFile
  */
 public class PackageInfo
 {
@@ -25,27 +31,60 @@ public class PackageInfo
 
   private final ControlFile controlFile;
 
+  /**
+   * Creates a new package info instance from the given control file.
+   *
+   * @param controlFile the Debian control file containing package metadata
+   */
   public PackageInfo(final ControlFile controlFile) {
     this.controlFile = controlFile;
   }
 
+  /**
+   * @return the underlying control file
+   */
   public ControlFile getControlFile() {
     return controlFile;
   }
 
+  /**
+   * @return the package name
+   * @throws NoSuchElementException if the package name field is not present
+   */
   public String getPackageName() {
     return getField(PACKAGE_FIELD);
   }
 
+  /**
+   * @return the package version
+   * @throws NoSuchElementException if the version field is not present
+   */
   public String getVersion() {
     return getField(VERSION_FIELD);
   }
 
+  /**
+   * @return the package architecture
+   * @throws NoSuchElementException if the architecture field is not present
+   */
   public String getArchitecture() {
     return getField(ARCHITECTURE_FIELD);
   }
 
+  /**
+   * Retrieves a field value from the control file.
+   *
+   * @param fieldName the name of the field to retrieve
+   * @return the field value
+   * @throws NoSuchElementException if the field is not present
+   */
   private String getField(final String fieldName) {
-    return controlFile.getField(fieldName).map(f -> f.value).get();
+    Optional<ControlFile.ControlField> fieldOptional = controlFile.getField(fieldName);
+    
+    // Using Java 21 pattern matching for instanceof
+    return switch (fieldOptional) {
+      case Optional<ControlFile.ControlField> opt when opt.isPresent() -> opt.get().value;
+      default -> throw new NoSuchElementException("Required field not found: " + fieldName);
+    };
   }
 }

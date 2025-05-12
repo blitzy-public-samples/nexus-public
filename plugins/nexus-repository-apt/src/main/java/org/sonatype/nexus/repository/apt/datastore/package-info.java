00e22c73d6e70f2840cbfe507d18f1f17afe65a8
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 07:34:59 +0530

    File: plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/package-info.java

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/package-info.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/package-info.java
@@ -10,8 +10,22 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-@FeatureFlag(name = DATASTORE_ENABLED)
 
+/**
+ * APT repository datastore implementation.
+ * <p>
+ * This package contains the datastore implementation for APT repositories.
+ * The implementation is compatible with Java 21 and leverages the following features:
+ * <ul>
+ *   <li>Virtual Threads for I/O operations when enabled via feature flag</li>
+ *   <li>Pattern Matching for type-safe data handling</li>
+ *   <li>Enhanced type checking with Java 21 compiler improvements</li>
+ * </ul>
+ *
+ * @since 3.30
+ * @updated 21.0 - Updated for Java 21 compatibility
+ */
+@FeatureFlag(name = DATASTORE_ENABLED)
 package org.sonatype.nexus.repository.apt.datastore;
 
 import org.sonatype.nexus.common.app.FeatureFlag;

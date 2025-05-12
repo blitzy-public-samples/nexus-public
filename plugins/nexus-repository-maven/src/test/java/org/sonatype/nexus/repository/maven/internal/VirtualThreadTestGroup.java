92548e04cc340a085946ea9913c7efe23716210a
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 05:38:52 +0530

    File: plugins/nexus-repository-maven/src/test/java/org/sonatype/nexus/repository/maven/internal/VirtualThreadTestGroup.java

---
+++ plugins/nexus-repository-maven/src/test/java/org/sonatype/nexus/repository/maven/internal/VirtualThreadTestGroup.java
@@ -0,0 +1,38 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.repository.maven.internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marker annotation for tests that specifically validate Java 21 virtual threads functionality.
+ * <p>
+ * This annotation can be used to categorize and selectively run tests that verify
+ * the behavior of code using Java 21's virtual threads (Project Loom) feature.
+ * <p>
+ * Tests marked with this annotation will be included in virtual thread test suites
+ * and can be filtered using JUnit's tag-based filtering mechanisms.
+ * <p>
+ * Virtual threads are lightweight threads that are managed by the JVM rather than the OS,
+ * allowing for high throughput in I/O-bound applications with many concurrent tasks.
+ *
+ * @since 3.77.0
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface VirtualThreadTestGroup {
+}

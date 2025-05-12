c99eb020df953946341707b9b4f12034813e4979
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 23:02:09 +0530

    File: testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/GolangHostedRepositoryConfig.java

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/GolangHostedRepositoryConfig.java
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/repository/config/GolangHostedRepositoryConfig.java
@@ -18,13 +18,34 @@ import org.sonatype.nexus.repository.Repository;
 
 import static org.sonatype.nexus.testsuite.testsupport.system.RepositoryTestSystem.FORMAT_GOLANG;
 
+/**
+ * Configuration for a hosted Golang repository in tests.
+ * <p>
+ * This class extends {@link HostedRepositoryConfigSupport} to provide a fluent builder pattern
+ * for configuring Golang hosted repository instances in test environments.
+ * <p>
+ * Hosted Golang repositories store Go modules locally rather than proxying from a remote source.
+ * <p>
+ * Compatible with Java 21 and supports testing with JUnit Jupiter 5.10.1 and Mockito 4.11.0.
+ * When running on Java 21, repository operations may benefit from Virtual Threads for improved concurrency.
+ */
 public class GolangHostedRepositoryConfig
     extends HostedRepositoryConfigSupport<GolangHostedRepositoryConfig>
 {
+  /**
+   * Constructs a new GolangHostedRepositoryConfig with the specified factory function.
+   * 
+   * @param factory The function that creates a Repository instance from this configuration
+   */
   public GolangHostedRepositoryConfig(final Function<GolangHostedRepositoryConfig, Repository> factory) {
     super(factory);
   }
 
+  /**
+   * Gets the format name for this repository type.
+   *
+   * @return The Golang format name
+   */
   @Override
   public String getFormat() {
     return FORMAT_GOLANG;

adea47325c8be2775e403a1a2b69b27205aee708
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 22:03:45 +0530

    File: testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/raw/RawClient.java

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/raw/RawClient.java
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/raw/RawClient.java
@@ -14,7 +14,9 @@ package org.sonatype.nexus.testsuite.testsupport.raw;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
 import org.sonatype.nexus.testsuite.testsupport.FormatClientSupport;
 
@@ -35,10 +37,26 @@ import static org.sonatype.nexus.repository.http.HttpMethods.MKCOL;
 
 /**
  * A simple test client for Raw repositories.
+ * <p>
+ * This implementation leverages Java 21 virtual threads for I/O-bound operations to improve
+ * throughput and scalability during testing. Virtual threads provide lightweight concurrency
+ * for operations that spend most of their time waiting for I/O, such as HTTP requests.
+ * <p>
+ * The client inherits virtual thread support from {@link FormatClientSupport} which handles
+ * the execution of HTTP requests on virtual threads.
+ *
+ * @since 3.60 Updated for Java 21 with virtual threads support
  */
 public class RawClient
     extends FormatClientSupport
 {
+  /**
+   * Creates a new RawClient instance.
+   *
+   * @param httpClient the HTTP client to use for requests
+   * @param httpClientContext the HTTP client context
+   * @param repositoryBaseUri the base URI of the repository
+   */
   public RawClient(final CloseableHttpClient httpClient,
                    final HttpClientContext httpClientContext,
                    final URI repositoryBaseUri)
@@ -46,6 +64,17 @@ public class RawClient
     super(httpClient, httpClientContext, repositoryBaseUri);
   }
 
+  /**
+   * Uploads a file to the specified path in the repository.
+   * <p>
+   * This method leverages virtual threads for I/O operations.
+   *
+   * @param path the path where the file should be uploaded
+   * @param contentType the content type of the file
+   * @param file the file to upload
+   * @return the HTTP status code of the response
+   * @throws Exception if an error occurs during the upload
+   */
   public int put(final String path, final ContentType contentType, final File file) throws Exception {
     checkNotNull(path);
     checkNotNull(file);
@@ -56,6 +85,16 @@ public class RawClient
     return status(execute(put));
   }
 
+  /**
+   * Uploads content to the specified path in the repository.
+   * <p>
+   * This method leverages virtual threads for I/O operations.
+   *
+   * @param path the path where the content should be uploaded
+   * @param entity the HTTP entity containing the content to upload
+   * @return the HTTP response
+   * @throws IOException if an error occurs during the upload
+   */
   public CloseableHttpResponse put(final String path, final HttpEntity entity) throws IOException {
     final URI uri = resolve(path);
     final HttpPut put = new HttpPut(uri);
@@ -63,14 +102,41 @@ public class RawClient
     return execute(put);
   }
 
+  /**
+   * Gets the content at the specified path as a byte array.
+   * <p>
+   * This method leverages virtual threads for I/O operations.
+   *
+   * @param path the path to retrieve
+   * @return the content as a byte array
+   * @throws Exception if an error occurs during the retrieval
+   */
   public byte[] getBytes(final String path) throws Exception {
     return bytes(get(path));
   }
 
+  /**
+   * Deletes the content at the specified path.
+   * <p>
+   * This method leverages virtual threads for I/O operations.
+   *
+   * @param path the path to delete
+   * @return the HTTP response
+   * @throws Exception if an error occurs during the deletion
+   */
   public HttpResponse delete(final String path) throws Exception {
     return execute(new HttpDelete(resolve(path)));
   }
 
+  /**
+   * Creates a collection at the specified path using the MKCOL method.
+   * <p>
+   * This method leverages virtual threads for I/O operations.
+   *
+   * @param path the path where the collection should be created
+   * @return the HTTP response
+   * @throws Exception if an error occurs during the collection creation
+   */
   public HttpResponse mkcol(final String path) throws Exception {
     HttpUriRequest mkcolRequest = RequestBuilder.create(MKCOL)
         .setUri(resolve(path)).build();

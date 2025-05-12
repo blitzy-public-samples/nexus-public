7a3a39a2c0bd087bd89bda3beb768023faebff9d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 16:34:05 +0530

    File: plugins/nexus-example-content/src/main/java/org/sonatype/nexus/content/example/internal/recipe/ExampleFormatSecurityContributor.java

--- plugins/nexus-example-content/src/main/java/org/sonatype/nexus/content/example/internal/recipe/ExampleFormatSecurityContributor.java
+++ plugins/nexus-example-content/src/main/java/org/sonatype/nexus/content/example/internal/recipe/ExampleFormatSecurityContributor.java
@@ -12,9 +12,9 @@
  */
 package org.sonatype.nexus.content.example.internal.recipe;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
 
 import org.sonatype.nexus.repository.Format;
 import org.sonatype.nexus.repository.security.RepositoryFormatSecurityContributor;

8ec6b08063be2658d44a0ffd0a39c4b196293933
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 17:21:57 +0530

    File: testsuite/nexus-docker-testsupport/src/main/java/com/sonatype/nexus/docker/testsupport/conda/CondaClientITConfigFactory.java

--- testsuite/nexus-docker-testsupport/src/main/java/com/sonatype/nexus/docker/testsupport/conda/CondaClientITConfigFactory.java
+++ testsuite/nexus-docker-testsupport/src/main/java/com/sonatype/nexus/docker/testsupport/conda/CondaClientITConfigFactory.java
@@ -15,16 +15,29 @@ package com.sonatype.nexus.docker.testsupport.conda;
 import com.sonatype.nexus.docker.testsupport.framework.DockerContainerConfig;
 
 /**
- * Factory for creation of Conda required objects
+ * Factory for creation of Conda required objects.
+ * 
+ * <p>This class is compatible with Java 21 and uses string templates for efficient string construction.</p>
+ *
+ * @since 3.60
  */
 public class CondaClientITConfigFactory
 {
   private static final String IMAGE_CONDA = "docker-all.repo.sonatype.com/continuumio/miniconda3";
 
   private CondaClientITConfigFactory() {
+    // Private constructor to prevent instantiation
   }
 
+  /**
+   * Creates a Docker container configuration for Conda with the specified image tag.
+   *
+   * @param imageTag the tag for the Conda Docker image
+   * @return a configured {@link DockerContainerConfig} instance
+   */
   public static DockerContainerConfig createCondaConfig(final String imageTag) {
-    return DockerContainerConfig.builder(IMAGE_CONDA + ":" + imageTag).build();
+    // Using Java 21 string template for image name construction
+    String imageName = STR."{IMAGE_CONDA}:{imageTag}";
+    return DockerContainerConfig.builder(imageName).build();
   }
 }

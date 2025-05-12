6e7f7c9d9c78a4886bacdfbfddca6ca185d3c9a0
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 12:44:12 +0530

    File: plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/internal/rest/ScriptPrivilegeApiResourceV1.java

--- plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/internal/rest/ScriptPrivilegeApiResourceV1.java
+++ plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/internal/rest/ScriptPrivilegeApiResourceV1.java
@@ -14,10 +14,10 @@ package org.sonatype.nexus.script.plugin.internal.rest;
 
 import java.util.Map;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
-import javax.ws.rs.Path;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.Path;
 
 import org.sonatype.nexus.security.SecuritySystem;
 import org.sonatype.nexus.security.internal.rest.SecurityApiResourceV1;

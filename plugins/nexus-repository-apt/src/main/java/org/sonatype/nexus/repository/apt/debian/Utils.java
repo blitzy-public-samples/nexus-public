28b03151551ef7744588d8794f1741fd1b4c6f09
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 07:33:59 +0530

    File: plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/debian/Utils.java

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/debian/Utils.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/debian/Utils.java
@@ -9,17 +9,28 @@
  * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ *
+ * This file has been verified for compatibility with Java 21 as part of the Nexus Repository Java 21 migration project.
  */
 package org.sonatype.nexus.repository.apt.debian;
 
 /**
+ * Utility methods for Debian package handling.
+ * 
  * @since 3.17
  */
 public class Utils
 {
   private Utils(){
+    // Utility class, should not be instantiated
   }
 
+  /**
+   * Determines if a path represents a Debian package file based on its extension.
+   * 
+   * @param path the file path to check
+   * @return true if the path ends with .deb or .udeb, false otherwise
+   */
   public static boolean isDebPackageContentType(final String path) {
     return path.endsWith(".deb") || path.endsWith(".udeb");
   }

0b20e9229df2811a9e873fdabd521983f011f86d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 16:57:38 +0530

    File: testsuite/nexus-content-suite/src/test/java/org/sonatype/nexus/content/matcher/ComponentMatcher.java

--- testsuite/nexus-content-suite/src/test/java/org/sonatype/nexus/content/matcher/ComponentMatcher.java
+++ testsuite/nexus-content-suite/src/test/java/org/sonatype/nexus/content/matcher/ComponentMatcher.java
@@ -17,13 +17,29 @@ import org.sonatype.nexus.repository.content.Component;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 
+/**
+ * Provides Hamcrest matchers for {@link Component} objects to be used in JUnit Jupiter tests.
+ * <p>
+ * This class leverages Hamcrest 2.2 matchers for testing component properties.
+ * 
+ * @since 3.38
+ */
 public class ComponentMatcher
 {
+  /**
+   * Creates a matcher that matches when the examined {@link Component}'s name
+   * satisfies the specified matcher.
+   *
+   * @param matcher the matcher to apply to the component's name
+   * @return a matcher that matches when the examined component's name satisfies the specified matcher
+   */
   public static FeatureMatcher<Component, String> name(Matcher<String> matcher) {
     return new FeatureMatcher<Component, String>(matcher, "name", "name")
     {
       @Override
       protected String featureValueOf(Component actual) {
+        // In more complex scenarios, Java 21 pattern matching could be used here
+        // For example: if (actual instanceof CustomComponent custom) { return custom.getSpecialName(); }
         return actual.name();
       }
     };

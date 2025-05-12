5039dd0d7a30fef7763a406bbb872d05f58d3f4d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 13:19:45 +0530

    File: plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/validator/PemCertificate.java

--- plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/validator/PemCertificate.java
+++ plugins/nexus-ssl-plugin/src/main/java/com/sonatype/nexus/ssl/plugin/validator/PemCertificate.java
@@ -16,22 +16,25 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.RECORD_COMPONENT;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Validate PEM formatted certificate.
  *
+ * Compatible with Java 21 and Jakarta EE validation API.
+ *
  * @since 3.0
  */
-@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, RECORD_COMPONENT})
 @Retention(RUNTIME)
 @Constraint(validatedBy = PemCertificateValidator.class)
 @Documented

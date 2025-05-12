c5939e58c636d33ebc82bbb28570457b94867226
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 09:00:30 +0530

    File: plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/snapshot/AptFilterInputStream.java

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/snapshot/AptFilterInputStream.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/internal/snapshot/AptFilterInputStream.java
@@ -18,7 +18,12 @@ import java.io.IOException;
 import org.bouncycastle.bcpg.ArmoredInputStream;
 
 /**
+ * A FilterInputStream implementation that reads clear text data from an ArmoredInputStream.
+ * This class stops reading when the end of clear text is reached or when the underlying
+ * stream is exhausted.
+ *
  * @since 3.31
+ * @see ArmoredInputStream
  */
 public class AptFilterInputStream
     extends FilterInputStream
@@ -27,11 +32,27 @@ public class AptFilterInputStream
 
   private final ArmoredInputStream armoredIn;
 
+  /**
+   * Creates a new AptFilterInputStream that filters the specified ArmoredInputStream.
+   *
+   * @param armoredIn the ArmoredInputStream to filter
+   */
   public AptFilterInputStream(final ArmoredInputStream armoredIn) {
     super(armoredIn);
     this.armoredIn = armoredIn;
   }
 
+  /**
+   * Reads the next byte of data from this input stream. The value byte is
+   * returned as an {@code int} in the range {@code 0} to
+   * {@code 255}. If no byte is available because the end of the stream
+   * has been reached or the stream is not in clear text mode, the value
+   * {@code -1} is returned.
+   *
+   * @return the next byte of data, or {@code -1} if the end of the
+   *         stream is reached or the stream is not in clear text mode
+   * @throws IOException if an I/O error occurs
+   */
   @Override
   public int read() throws IOException {
     if (done) {
@@ -45,6 +66,23 @@ public class AptFilterInputStream
     return c;
   }
 
+  /**
+   * Reads up to {@code len} bytes of data from this input stream
+   * into an array of bytes. This method blocks until some input is
+   * available.
+   * <p>
+   * This method simply performs {@code read()} repeatedly until either
+   * {@code len} bytes have been read, the end of the stream has been reached,
+   * or an exception is thrown.
+   *
+   * @param b   the buffer into which the data is read
+   * @param off the start offset in the destination array {@code b}
+   * @param len the maximum number of bytes read
+   * @return the total number of bytes read into the buffer, or
+   *         {@code -1} if there is no more data because the end of
+   *         the stream has been reached
+   * @throws IOException if an I/O error occurs
+   */
   @Override
   public int read(byte[] b, int off, int len) throws IOException {
     for (int i = 0; i < len; i++) {

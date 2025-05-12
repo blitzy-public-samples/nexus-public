23327f7ac95dbd87661f8ac91671b7c3f2df146a
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 11:06:38 +0530

    File: plugins/nexus-repository-raw/src/main/java/org/sonatype/nexus/repository/raw/rest/RawHostedRepositoriesApiResourceV1.java

--- plugins/nexus-repository-raw/src/main/java/org/sonatype/nexus/repository/raw/rest/RawHostedRepositoriesApiResourceV1.java
+++ plugins/nexus-repository-raw/src/main/java/org/sonatype/nexus/repository/raw/rest/RawHostedRepositoriesApiResourceV1.java
@@ -12,15 +12,17 @@
  */
 package org.sonatype.nexus.repository.raw.rest;
 
-import javax.inject.Named;
-import javax.inject.Singleton;
-import javax.ws.rs.Path;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.Path;
 
 import org.sonatype.nexus.repository.rest.api.RepositoriesApiResourceV1;
 
 import static org.sonatype.nexus.repository.raw.rest.RawHostedRepositoriesApiResourceV1.RESOURCE_URI;
 
 /**
+ * REST API resource for Raw hosted repositories.
+ * 
  * @since 3.26
  */
 @Named

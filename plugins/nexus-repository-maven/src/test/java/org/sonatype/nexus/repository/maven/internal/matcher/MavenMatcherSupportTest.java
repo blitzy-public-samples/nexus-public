b54d7c12f8bcc392f4bc177b033ce8e49837a966
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 06:27:40 +0530

    File: plugins/nexus-repository-maven/src/test/java/org/sonatype/nexus/repository/maven/internal/matcher/MavenMatcherSupportTest.java

--- plugins/nexus-repository-maven/src/test/java/org/sonatype/nexus/repository/maven/internal/matcher/MavenMatcherSupportTest.java
+++ plugins/nexus-repository-maven/src/test/java/org/sonatype/nexus/repository/maven/internal/matcher/MavenMatcherSupportTest.java
@@ -12,51 +12,52 @@
  */
 package org.sonatype.nexus.repository.maven.internal.matcher;
 
+import java.util.function.Predicate;
+
 import org.sonatype.goodies.testsupport.TestSupport;
 
-import com.google.common.base.Predicate;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
- * UT for {@link MavenMatcherSupport}
+ * Tests for {@link MavenMatcherSupport}
  *
  * @since 3.0
  */
-public class MavenMatcherSupportTest
+class MavenMatcherSupportTest
     extends TestSupport
 {
   @Test
-  public void equalsWithHashes() {
+  void equalsWithHashes() {
     Predicate<String> pred = MavenMatcherSupport.withHashes("/some-path.txt"::equals);
 
-    assertThat(pred.apply("/some-path.txt"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha1"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.md5"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha256"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha512"), equalTo(true));
+    assertThat(pred.test("/some-path.txt"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha1"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.md5"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha256"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha512"), equalTo(true));
 
-    assertThat(pred.apply("some-path.txt"), equalTo(false));
-    assertThat(pred.apply("/some-path.txt.crc"), equalTo(false));
-    assertThat(pred.apply("/some-path.tx"), equalTo(false));
-    assertThat(pred.apply("/other-path.txt"), equalTo(false));
+    assertThat(pred.test("some-path.txt"), equalTo(false));
+    assertThat(pred.test("/some-path.txt.crc"), equalTo(false));
+    assertThat(pred.test("/some-path.tx"), equalTo(false));
+    assertThat(pred.test("/other-path.txt"), equalTo(false));
   }
 
   @Test
-  public void endsWithWithHashes() {
+  void endsWithWithHashes() {
     Predicate<String> pred = MavenMatcherSupport.withHashes((String input) -> input.endsWith("/some-path.txt"));
 
-    assertThat(pred.apply("/some/prefix/some-path.txt"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha1"), equalTo(true));
-    assertThat(pred.apply("some/prefix/some-path.txt.md5"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha256"), equalTo(true));
-    assertThat(pred.apply("/some-path.txt.sha512"), equalTo(true));
+    assertThat(pred.test("/some/prefix/some-path.txt"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha1"), equalTo(true));
+    assertThat(pred.test("some/prefix/some-path.txt.md5"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha256"), equalTo(true));
+    assertThat(pred.test("/some-path.txt.sha512"), equalTo(true));
 
-    assertThat(pred.apply("some-path.txt"), equalTo(false));
-    assertThat(pred.apply("/some-path.txt.crc"), equalTo(false));
-    assertThat(pred.apply("/some-path.tx"), equalTo(false));
-    assertThat(pred.apply("/other-path.txt"), equalTo(false));
+    assertThat(pred.test("some-path.txt"), equalTo(false));
+    assertThat(pred.test("/some-path.txt.crc"), equalTo(false));
+    assertThat(pred.test("/some-path.tx"), equalTo(false));
+    assertThat(pred.test("/other-path.txt"), equalTo(false));
   }
 }

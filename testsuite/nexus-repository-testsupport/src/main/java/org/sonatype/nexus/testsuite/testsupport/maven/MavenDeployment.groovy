35554f38f92b07622ad8d2ef14eea972594c8c8d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 19:44:19 +0530

    File: testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/maven/MavenDeployment.groovy

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/maven/MavenDeployment.groovy
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/maven/MavenDeployment.groovy
@@ -13,6 +13,9 @@
 package org.sonatype.nexus.testsuite.testsupport.maven
 
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.Executors
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.CompletableFuture
 
 import javax.annotation.Nullable
 
@@ -27,6 +30,8 @@ import static com.google.common.base.Preconditions.checkState
 
 /**
  * Describe and modify a Maven deployment based on some template files and the desired configuration.
+ * Optimized for Java 21 with Virtual Threads support for improved I/O operations.
+ * 
  * @since 3.2
  */
 @Builder
@@ -86,12 +91,13 @@ class MavenDeployment
   boolean ensureCleanOnInit = true
 
   /**
-   * Set the version used for Java, this may vary depending on the executing JVM
+   * Set the version used for Java, defaults to Java 21 for compatibility with Virtual Threads.
    */
-  String javaVersion = "1.8"
+  String javaVersion = "21"
 
   /**
    * Initialize a new maven project folder and configure pom.xml and settings.xml files as specified.
+   * Uses Java 21 Virtual Threads for improved I/O performance when available.
    */
   public MavenDeployment init() {
     checkState(projectTemplateDir.isDirectory(), "Project template dir is missing: $projectTemplateDir")
@@ -105,31 +111,51 @@ class MavenDeployment
 
     log.info this.toString()
 
-    DirectoryHelper.copy(projectTemplateDir.toPath(), projectDir.toPath())
+    // Use Virtual Threads for file operations when running on Java 21
+    try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      // Copy project files using Virtual Threads
+      CompletableFuture.runAsync(() -> {
+        DirectoryHelper.copy(projectTemplateDir.toPath(), projectDir.toPath())
+      }, executor).join()
 
-    String settingsContent = Files.toString(settingsTemplate, StandardCharsets.UTF_8)
-        .replace('${proxyUrl}', proxyUrl.toString())
-    File settings = settingsFile()
-    settings.createNewFile()
-    settings.text = settingsContent
+      // Process settings.xml and pom.xml in parallel
+      CompletableFuture<Void> settingsTask = CompletableFuture.runAsync(() -> {
+        String settingsContent = Files.toString(settingsTemplate, StandardCharsets.UTF_8)
+            .replace('${proxyUrl}', proxyUrl.toString())
+        File settings = settingsFile()
+        settings.createNewFile()
+        settings.text = settingsContent
+      }, executor)
 
-    File pom = pomFile()
-    String pomContent = Files.toString(pom, StandardCharsets.UTF_8)
-        .replace('${project.groupId}', groupId)
-        .replace('${project.artifactId}', artifactId)
-        .replace('${project.version}', version)
-    if(siteUrl) {
-      pomContent = pomContent.replace('${site.url}', siteUrl.authority + siteUrl.path)
+      CompletableFuture<Void> pomTask = CompletableFuture.runAsync(() -> {
+        File pom = pomFile()
+        String pomContent = Files.toString(pom, StandardCharsets.UTF_8)
+            .replace('${project.groupId}', groupId)
+            .replace('${project.artifactId}', artifactId)
+            .replace('${project.version}', version)
+        if(siteUrl) {
+          pomContent = pomContent.replace('${site.url}', siteUrl.authority + siteUrl.path)
+        }
+        pom.text = pomContent
+      }, executor)
+
+      // Wait for both tasks to complete
+      CompletableFuture.allOf(settingsTask, pomTask).join()
     }
-    pom.text = pomContent
 
     return this
   }
 
+  /**
+   * Get the pom.xml file in the project directory.
+   */
   public File pomFile() {
     new File(projectDir, 'pom.xml')
   }
 
+  /**
+   * Get the settings.xml file in the project directory.
+   */
   public File settingsFile() {
     new File(projectDir, 'settings.xml')
   }

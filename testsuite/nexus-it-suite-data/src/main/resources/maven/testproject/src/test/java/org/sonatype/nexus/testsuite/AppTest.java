2771fc7e6202560c74bb8f4ca0b6e63471b000bb
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 17:59:56 +0530

    File: testsuite/nexus-it-suite-data/src/main/resources/maven/testproject/src/test/java/org/sonatype/nexus/testsuite/AppTest.java

--- testsuite/nexus-it-suite-data/src/main/resources/maven/testproject/src/test/java/org/sonatype/nexus/testsuite/AppTest.java
+++ testsuite/nexus-it-suite-data/src/main/resources/maven/testproject/src/test/java/org/sonatype/nexus/testsuite/AppTest.java
@@ -12,39 +12,21 @@
  */
 package org.sonatype.nexus.testsuite;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
+    @Test
+    public void appWorksCorrectly()
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        assertTrue(true, "App should work correctly");
     }
 }

22a38d9014a874fc6a72902a76ce7e01e0684d27
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-12 07:48:22 +0530

    File: plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/api/AptHostedRepositoriesAttributes.java

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/api/AptHostedRepositoriesAttributes.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/api/AptHostedRepositoriesAttributes.java
@@ -12,7 +12,6 @@
  */
 package org.sonatype.nexus.repository.apt.api;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotEmpty;
@@ -21,19 +20,11 @@ import javax.validation.constraints.NotEmpty;
  * REST API model of apt-specific hosted configuration.
  *
  * @since 3.20
+ * @since 3.60 Converted to Java Record for Java 21 compatibility
  */
-public class AptHostedRepositoriesAttributes
-{
-  @ApiModelProperty(value = "Distribution to fetch", example = "bionic")
-  @NotEmpty
-  protected final String distribution;
-
-  @JsonCreator
-  public AptHostedRepositoriesAttributes(@JsonProperty("distribution") final String distribution) {
-    this.distribution = distribution;
-  }
-
-  public String getDistribution() {
-    return distribution;
-  }
-}
+public record AptHostedRepositoriesAttributes(
+    @ApiModelProperty(value = "Distribution to fetch", example = "bionic")
+    @NotEmpty
+    @JsonProperty("distribution")
+    String distribution
+) {}

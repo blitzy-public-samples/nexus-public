42ee90972f9506e1f8dfa93436059ffc5fed27b1
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-19 14:53:34 +0530

    File: components/nexus-blobstore/src/test/java/org/sonatype/nexus/blobstore/virtualthread/VirtualThreadTestGroup.java

---
+++ components/nexus-blobstore/src/test/java/org/sonatype/nexus/blobstore/virtualthread/VirtualThreadTestGroup.java
@@ -0,0 +1,34 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.blobstore.virtualthread;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Annotation for tests that should be run using virtual threads.
+ * This is a composed annotation that includes both @Test and @Tag("virtualthread").
+ *
+ * @since 3.60
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Test
+@Tag("virtualthread")
+public @interface VirtualThreadTestGroup {
+}

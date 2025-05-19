006f095db8e0b232c86569a5f56a243fc86a407d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 02:04:40 +0530

    File: components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/event/component/ComponentPurgedEvent.java

--- components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/event/component/ComponentPurgedEvent.java
+++ components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/event/component/ComponentPurgedEvent.java
@@ -12,12 +12,15 @@
  */
 package org.sonatype.nexus.repository.content.event.component;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.SequencedCollection;
 
 import org.sonatype.nexus.repository.content.Component;
 import org.sonatype.nexus.repository.content.store.ContentStoreEvent;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Collections.unmodifiableSequencedCollection;
 
 /**
  * Event sent whenever a large number of {@link Component}s are purged along with their assets.
@@ -27,21 +30,83 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class ComponentPurgedEvent
     extends ContentStoreEvent
 {
-  private final int[] componentIds;
+  private final SequencedCollection<Integer> componentIds;
 
-  public ComponentPurgedEvent(final int contentRepositoryId, final int[] componentIds) { // NOSONAR
+  /**
+   * Creates a new event with the given component IDs.
+   *
+   * @param contentRepositoryId the repository ID
+   * @param componentIds the IDs of the purged components
+   */
+  public ComponentPurgedEvent(final int contentRepositoryId, final int[] componentIds) {
     super(contentRepositoryId);
-    this.componentIds = checkNotNull(componentIds);
+    checkNotNull(componentIds);
+    
+    // Convert array to SequencedCollection
+    ArrayList<Integer> idList = new ArrayList<>(componentIds.length);
+    for (int id : componentIds) {
+      idList.add(id);
+    }
+    this.componentIds = unmodifiableSequencedCollection(idList);
   }
 
-  public int[] getComponentIds() {
-    return componentIds; // NOSONAR
+  /**
+   * Creates a new event with the given component IDs.
+   *
+   * @param contentRepositoryId the repository ID
+   * @param componentIds the collection of purged component IDs
+   */
+  public ComponentPurgedEvent(final int contentRepositoryId, final Collection<Integer> componentIds) {
+    super(contentRepositoryId);
+    checkNotNull(componentIds);
+    
+    // Convert collection to SequencedCollection if needed
+    if (componentIds instanceof SequencedCollection) {
+      this.componentIds = unmodifiableSequencedCollection((SequencedCollection<Integer>) componentIds);
+    } else {
+      this.componentIds = unmodifiableSequencedCollection(new ArrayList<>(componentIds));
+    }
+  }
+
+  /**
+   * Returns the IDs of the purged components as a SequencedCollection.
+   * 
+   * @return sequenced collection of component IDs
+   */
+  public SequencedCollection<Integer> getComponentIds() {
+    return componentIds;
+  }
+  
+  /**
+   * Returns the IDs of the purged components as an array for backward compatibility.
+   * 
+   * @return array of component IDs
+   * @deprecated Use {@link #getComponentIds()} instead which returns a SequencedCollection
+   */
+  @Deprecated
+  public int[] getComponentIdsArray() {
+    int[] result = new int[componentIds.size()];
+    int i = 0;
+    for (Integer id : componentIds) {
+      result[i++] = id;
+    }
+    return result;
+  }
+
+  /**
+   * Checks if the event contains the specified component ID.
+   *
+   * @param componentId the component ID to check
+   * @return true if the event contains the component ID, false otherwise
+   */
+  public boolean containsComponentId(int componentId) {
+    return componentIds.contains(componentId);
   }
 
   @Override
   public String toString() {
     return "ComponentPurgedEvent{" +
-        "componentIds=" + Arrays.toString(componentIds) +
+        "componentIds=" + componentIds +
         "} " + super.toString();
   }
 }

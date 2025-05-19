66505213b7242279f5cc1961931974d1ff8e8ee4
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-19 15:45:18 +0530

    File: components/nexus-blobstore-file/src/test/java/org/sonatype/nexus/blobstore/file/internal/FileBlobStoreDescriptorTest.java

--- components/nexus-blobstore-file/src/test/java/org/sonatype/nexus/blobstore/file/internal/FileBlobStoreDescriptorTest.java
+++ components/nexus-blobstore-file/src/test/java/org/sonatype/nexus/blobstore/file/internal/FileBlobStoreDescriptorTest.java
@@ -15,16 +15,17 @@ package org.sonatype.nexus.blobstore.file.internal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.sonatype.goodies.testsupport.TestSupport;
 import org.sonatype.nexus.blobstore.BlobStoreUtil;
 import org.sonatype.nexus.blobstore.MockBlobStoreConfiguration;
 import org.sonatype.nexus.blobstore.api.BlobStoreConfiguration;
 import org.sonatype.nexus.blobstore.quota.BlobStoreQuotaService;
 import org.sonatype.nexus.common.app.ApplicationDirectories;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -34,8 +35,8 @@ import static org.mockito.Mockito.when;
 import static org.sonatype.nexus.blobstore.file.FileBlobStore.CONFIG_KEY;
 import static org.sonatype.nexus.blobstore.file.FileBlobStore.PATH_KEY;
 
+@ExtendWith(MockitoExtension.class)
 public class FileBlobStoreDescriptorTest
-    extends TestSupport
 {
   @Mock
   BlobStoreQuotaService quotaService;
@@ -53,15 +54,15 @@ public class FileBlobStoreDescriptorTest
 
   private static final int MAX_NAME_LENGTH = 255;
 
-  @Before
-  public void setup() {
+  @BeforeEach
+  void setup() {
     descriptor =
         new FileBlobStoreDescriptor(quotaService, applicationDirectories, blobStoreUtil, fileBlobStorePathValidator);
     when(blobStoreUtil.validateFilePath(anyString(), anyInt())).thenReturn(true);
   }
 
   @Test
-  public void descriptorValidationCallQuotaValidation() throws Exception {
+  void descriptorValidationCallQuotaValidation() throws Exception {
     BlobStoreConfiguration config = new MockBlobStoreConfiguration();
     Path tempDir = Files.createTempDirectory("test");
     config.attributes(CONFIG_KEY).set(PATH_KEY, tempDir.toAbsolutePath().toString());
@@ -70,12 +71,11 @@ public class FileBlobStoreDescriptorTest
   }
 
   @Test
-  public void descriptorValidationCallPathValidation() throws Exception {
+  void descriptorValidationCallPathValidation() throws Exception {
     BlobStoreConfiguration config = new MockBlobStoreConfiguration();
     String tempDir = Files.createTempDirectory("test").toString();
     config.attributes(CONFIG_KEY).set(PATH_KEY, tempDir);
     descriptor.validateConfig(config);
     verify(blobStoreUtil, times(1)).validateFilePath(tempDir, MAX_NAME_LENGTH);
   }
-
 }

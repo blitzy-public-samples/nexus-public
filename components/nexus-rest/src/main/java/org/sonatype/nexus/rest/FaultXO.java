eb07c3d96fc7a332e4bb19c736053ebc08e5bfba
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 15:56:24 +0530

    File: components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java

--- components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java
+++ components/nexus-rest/src/main/java/org/sonatype/nexus/rest/FaultXO.java
@@ -18,61 +18,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Fault exchange object.
- * 
- * Implemented as a Java 21 record for more concise and efficient error representation.
- * The record pattern provides immutability and automatic implementations of
- * equals(), hashCode(), and canonical constructor.
  *
  * @since 3.0
  */
 @XmlRootElement(name = "fault")
 public record FaultXO(
-    @JsonProperty("id")
-    String id,
-    
-    @JsonProperty("message")
-    String message
+  @JsonProperty
+  String id,
+
+  @JsonProperty
+  String message
 ) {
   /**
-   * No-args constructor for deserialization support.
-   * Required by some frameworks like Jackson and JAXB.
+   * Default constructor for deserialization.
    */
   public FaultXO() {
     this(null, null);
   }
-  
-  /**
-   * Static factory method for creating an empty instance.
-   * 
-   * @return A new FaultXO instance with null values
-   */
-  public static FaultXO empty() {
-    return new FaultXO(null, null);
-  }
 
   /**
-   * Constructor with Throwable as the message source.
+   * Constructor with a cause.
    */
   public FaultXO(final String id, final Throwable cause) {
     this(id, cause.toString());
-  }
-  
-  /**
-   * Static factory method with Throwable as the message source.
-   * 
-   * @param id The fault identifier
-   * @param cause The throwable cause
-   * @return A new FaultXO instance
-   */
-  public static FaultXO fromThrowable(final String id, final Throwable cause) {
-    return new FaultXO(id, cause.toString());
-  }
-
-  @Override
-  public String toString() {
-    return getClass().getSimpleName() + "{"
-        + "id='" + id + '\''
-        + ", message='" + message + '\''
-        + '}';
   }
 }

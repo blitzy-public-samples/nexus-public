574250f390cfb59d6221beac016cc006f0d731ab
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 10:07:19 +0530

    File: components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/resources/doc/RepositoriesResourceDoc.java

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/resources/doc/RepositoriesResourceDoc.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/resources/doc/RepositoriesResourceDoc.java
@@ -1,1 +1,54 @@
-Failed to process file
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.rest.internal.resources.doc;
+
+import java.util.List;
+
+import org.sonatype.nexus.repository.rest.api.RepositoryXO;
+import org.sonatype.nexus.repository.rest.internal.resources.RepositoriesResource;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import static org.sonatype.nexus.rest.ApiDocConstants.API_REPOSITORY_MANAGEMENT;
+import static org.sonatype.nexus.rest.ApiDocConstants.AUTHENTICATION_REQUIRED;
+import static org.sonatype.nexus.rest.ApiDocConstants.INSUFFICIENT_PERMISSIONS;
+import static org.sonatype.nexus.rest.ApiDocConstants.REPOSITORY_NOT_FOUND;
+
+/**
+ * Swagger documentation for {@link RepositoriesResource}
+ * 
+ * This interface is compatible with Java 21 Virtual Threads, allowing repository operations
+ * to be executed efficiently in high-concurrency scenarios. The underlying implementation
+ * can leverage Virtual Threads to handle I/O-bound operations without blocking platform threads,
+ * resulting in improved scalability and resource utilization.
+ *
+ * @since 3.9
+ */
+@Tag(name = API_REPOSITORY_MANAGEMENT)
+public interface RepositoriesResourceDoc
+{
+  @Operation(summary = "List repositories")
+  List<RepositoryXO> getRepositories();
+
+  @Operation(summary = "Get repository details")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "401", description = AUTHENTICATION_REQUIRED),
+      @ApiResponse(responseCode = "403", description = INSUFFICIENT_PERMISSIONS),
+      @ApiResponse(responseCode = "404", description = REPOSITORY_NOT_FOUND)
+  })
+  RepositoryXO getRepository(@Parameter(description = "Name of the repository to get") final String repositoryName);
+}

a5d6610a8b361af1cc1a540a652627f14ff3fa41
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 17:01:43 +0530

    File: components/nexus-security/src/main/java/org/sonatype/nexus/security/config/AdminPasswordFileManager.java

--- components/nexus-security/src/main/java/org/sonatype/nexus/security/config/AdminPasswordFileManager.java
+++ components/nexus-security/src/main/java/org/sonatype/nexus/security/config/AdminPasswordFileManager.java
@@ -15,17 +15,45 @@ package org.sonatype.nexus.security.config;
 import java.io.IOException;
 
 /**
+ * Manager for the admin password file.
+ * 
  * @since 3.17
  */
 public interface AdminPasswordFileManager
 {
+  /**
+   * Check if the admin password file exists.
+   *
+   * @return true if the file exists, false otherwise
+   */
   boolean exists();
 
+  /**
+   * Get the absolute path to the admin password file.
+   *
+   * @return the absolute path to the admin password file
+   */
   String getPath();
 
+  /**
+   * Write the password to the admin password file.
+   *
+   * @param password the password to write
+   * @return true if the write was successful, false otherwise
+   * @throws IOException if an I/O error occurs
+   */
   boolean writeFile(String password) throws IOException;
 
+  /**
+   * Read the password from the admin password file.
+   *
+   * @return the password from the file, or null if the file doesn't exist
+   * @throws IOException if an I/O error occurs
+   */
   String readFile() throws IOException;
 
+  /**
+   * Remove the admin password file if it exists.
+   */
   void removeFile();
 }

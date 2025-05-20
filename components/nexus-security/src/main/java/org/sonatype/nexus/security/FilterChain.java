36dd84bbfa2f0ab2ba2f1dc06c61fb8653336bf7
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 16:39:46 +0530

    File: components/nexus-security/src/main/java/org/sonatype/nexus/security/FilterChain.java

--- components/nexus-security/src/main/java/org/sonatype/nexus/security/FilterChain.java
+++ components/nexus-security/src/main/java/org/sonatype/nexus/security/FilterChain.java
@@ -12,37 +12,21 @@
  */
 package org.sonatype.nexus.security;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 /**
- * A Shiro filter chain (mapping between a path pattern and a filter expression).
+ * Describes a chain of filters to apply to a specific URL path pattern.
+ * Updated for Java 21 compatibility.
  *
- * @since 2.5
+ * @since 3.0
  */
-public class FilterChain
+public interface FilterChain
 {
-  private final String pathPattern;
+  /**
+   * @return URL path pattern to match
+   */
+  String getPathPattern();
 
-  private final String filterExpression;
-
-  public FilterChain(final String pathPattern, final String filterExpression) {
-    this.pathPattern = checkNotNull(pathPattern);
-    this.filterExpression = checkNotNull(filterExpression);
-  }
-
-  public String getPathPattern() {
-    return pathPattern;
-  }
-
-  public String getFilterExpression() {
-    return filterExpression;
-  }
-
-  @Override
-  public String toString() {
-    return getClass().getSimpleName() + "{" +
-        "pathPattern='" + pathPattern + '\'' +
-        ", filterExpression='" + filterExpression + '\'' +
-        '}';
-  }
+  /**
+   * @return Filter expression to apply
+   */
+  String getFilterExpression();
 }

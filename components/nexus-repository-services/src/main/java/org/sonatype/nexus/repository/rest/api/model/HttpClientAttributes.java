0ad58e94b708a5c251cac3b99e54b7282c8546ff
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 08:56:51 +0530

    File: components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/model/HttpClientAttributes.java

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/model/HttpClientAttributes.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/model/HttpClientAttributes.java
@@ -1,1 +1,55 @@
-Failed to process file
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.rest.api.model;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * REST API model for describing HTTP connection properties for proxy repositories.
+ *
+ * @since 3.20
+ */
+public record HttpClientAttributes(
+    @ApiModelProperty(value = "Whether to block outbound connections on the repository", example = "false")
+    @NotNull
+    Boolean blocked,
+
+    @ApiModelProperty(
+        value = "Whether to auto-block outbound connections if remote peer is detected as unreachable/unresponsive",
+        example = "true")
+    @NotNull
+    Boolean autoBlock,
+
+    @Valid
+    HttpClientConnectionAttributes connection,
+
+    @Valid
+    HttpClientConnectionAuthenticationAttributes authentication
+) {
+  @JsonCreator
+  public HttpClientAttributes(
+      @JsonProperty("blocked") final Boolean blocked,
+      @JsonProperty("autoBlock") final Boolean autoBlock,
+      @JsonProperty("connection") final HttpClientConnectionAttributes connection,
+      @JsonProperty("authentication") final HttpClientConnectionAuthenticationAttributes authentication) {
+    this.blocked = blocked;
+    this.autoBlock = autoBlock;
+    this.connection = connection;
+    this.authentication = authentication;
+  }
+}

e0d9fdcbc1090063e3d95559c4e06edb17a4f2c1
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-20 09:52:19 +0530

    File: components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/api/RepositoryXO.java

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/api/RepositoryXO.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/internal/api/RepositoryXO.java
@@ -1,1 +1,22 @@
-Failed to process file
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.rest.internal.api;
+
+/**
+ * Repository transfer object for REST API responses.
+ * 
+ * @since 3.29
+ */
+public record RepositoryXO(String id, String name) {
+  // Record automatically provides constructor, accessors, equals, hashCode, and toString
+}

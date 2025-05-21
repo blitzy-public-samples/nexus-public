640625edc3d5c709552e93ee5eea98153a84f4f9
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-21 15:01:58 +0530

    File: components/nexus-base/src/main/java/org/sonatype/nexus/internal/security/model/CUserDAO.java

--- components/nexus-base/src/main/java/org/sonatype/nexus/internal/security/model/CUserDAO.java
+++ components/nexus-base/src/main/java/org/sonatype/nexus/internal/security/model/CUserDAO.java
@@ -12,15 +12,62 @@
  */
 package org.sonatype.nexus.internal.security.model;
 
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Param;
 import org.sonatype.nexus.datastore.api.IdentifiedDataAccess;
 
 /**
  * {@link CUserData} access.
+ *
+ * <p>This interface is compatible with Java 21 Virtual Threads for database operations,
+ * allowing for high-concurrency database access with minimal resource usage.</p>
  *
  * @since 3.21
  */
 public interface CUserDAO
     extends IdentifiedDataAccess<CUserData>
 {
-  // no additional behaviour...
+  /**
+   * Browse existing user entities.
+   *
+   * @return an iterable of all user entities
+   */
+  @Override
+  Iterable<CUserData> browse();
+
+  /**
+   * Create a new user entity.
+   *
+   * @param entity the user entity to create
+   */
+  @Override
+  void create(@Param("entity") CUserData entity);
+
+  /**
+   * Retrieve the user entity with the given id.
+   *
+   * @param id the id of the user entity to retrieve
+   * @return an optional containing the user entity if found
+   */
+  @Override
+  Optional<CUserData> read(@Param("id") String id);
+
+  /**
+   * Update an existing user entity.
+   *
+   * @param entity the user entity to update
+   * @return true if the entity was updated, false otherwise
+   */
+  @Override
+  boolean update(@Param("entity") CUserData entity);
+
+  /**
+   * Delete the user entity with the given id.
+   *
+   * @param id the id of the user entity to delete
+   * @return true if the entity was deleted, false otherwise
+   */
+  @Override
+  boolean delete(@Param("id") String id);
 }

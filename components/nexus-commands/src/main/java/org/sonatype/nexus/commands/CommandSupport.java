e95dd3e9c8f1e271ddc039d823a9202ba0881b91
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-21 21:17:07 +0530

    File: components/nexus-commands/src/main/java/org/sonatype/nexus/commands/CommandSupport.java

--- components/nexus-commands/src/main/java/org/sonatype/nexus/commands/CommandSupport.java
+++ components/nexus-commands/src/main/java/org/sonatype/nexus/commands/CommandSupport.java
@@ -13,24 +13,54 @@
 package org.sonatype.nexus.commands;
 
 import org.apache.karaf.shell.api.action.Action;
-import org.apache.karaf.shell.api.console.Command;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.console.Command as ConsoleCommand;
 import org.apache.karaf.shell.api.console.Session;
 import org.apache.karaf.shell.impl.action.command.ActionCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Import for Java 21 String Templates
+import static java.lang.StringTemplate.STR;
+
 /**
- * Provides support for Karaf {@link Action} {@link Command} implementations.
+ * Provides support for Karaf {@link Action} {@link ConsoleCommand} implementations.
  * 
  * @since 3.0
+ * @see ActionCommand
+ * @see Action
+ * @see Command
+ * @see ConsoleCommand
+ * @see Session
+ * 
+ * @compatible with Java 21 and Karaf 4.4.4
  */
 public abstract class CommandSupport
     extends ActionCommand
 {
+  /**
+   * Logger for this class, using Java 21 String Templates for logging.
+   * <p>
+   * Example usage in subclasses:
+   * <pre>
+   * String name = "command";
+   * int count = 5;
+   * log.debug(STR."Executing \{name} with \{count} parameters");
+   * </pre>
+   * </p>
+   */
   protected final Logger log = LoggerFactory.getLogger(getClass());
 
+  /**
+   * Creates a new command support instance.
+   * 
+   * @param actionClass the action class to support, must be annotated with {@link Command}
+   * @throws IllegalArgumentException if actionClass is not annotated with {@link Command}
+   */
   public CommandSupport(final Class<? extends Action> actionClass) {
     super(null, actionClass);
+    // Note: ActionCommand constructor in Karaf 4.4.4 validates that actionClass has @Command annotation
+    // so we don't need to explicitly check it here
   }
 
   @Override

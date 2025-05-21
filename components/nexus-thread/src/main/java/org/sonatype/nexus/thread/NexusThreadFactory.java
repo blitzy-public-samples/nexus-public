4fb299f245c8cb9a42c2b56747a7753cbe6d70a3
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-21 11:09:00 +0530

    File: components/nexus-thread/src/main/java/org/sonatype/nexus/thread/NexusThreadFactory.java

--- components/nexus-thread/src/main/java/org/sonatype/nexus/thread/NexusThreadFactory.java
+++ components/nexus-thread/src/main/java/org/sonatype/nexus/thread/NexusThreadFactory.java
@@ -33,6 +33,10 @@ public class NexusThreadFactory
 
   private int threadPriority;
 
+  private boolean useVirtualThreads;
+
+  private boolean inheritInheritableThreadLocals;
+
   public NexusThreadFactory(String poolId, String threadGroupName) {
     this(poolId, threadGroupName, Thread.NORM_PRIORITY);
   }
@@ -47,17 +51,115 @@ public class NexusThreadFactory
       final int threadPriority,
       final boolean daemonThread)
   {
+    this(poolId, threadGroupName, threadPriority, daemonThread, false);
+  }
+
+  public NexusThreadFactory(
+      final String poolId,
+      final String threadGroupName,
+      final int threadPriority,
+      final boolean daemonThread,
+      final boolean useVirtualThreads)
+  {
+    this(poolId, threadGroupName, threadPriority, daemonThread, useVirtualThreads, true);
+  }
+
+  public NexusThreadFactory(
+      final String poolId,
+      final String threadGroupName,
+      final int threadPriority,
+      final boolean daemonThread,
+      final boolean useVirtualThreads,
+      final boolean inheritInheritableThreadLocals)
+  {
     int poolNum = poolNumber.getAndIncrement();
     this.schedulerThreadGroup = new ThreadGroup(threadGroupName + " #" + poolNum);
     this.namePrefix = poolId + "-" + poolNum + "-thread-";
     this.deamonThread = daemonThread;
     this.threadPriority = threadPriority;
+    this.useVirtualThreads = useVirtualThreads;
+    this.inheritInheritableThreadLocals = inheritInheritableThreadLocals;
   }
 
   public Thread newThread(final Runnable r) {
+    if (useVirtualThreads) {
+      return createVirtualThread(r);
+    } else {
+      return createPlatformThread(r);
+    }
+  }
+
+  /**
+   * Creates a platform thread with the configured properties.
+   *
+   * @param r the Runnable to be executed by the thread
+   * @return a new platform thread
+   */
+  private Thread createPlatformThread(final Runnable r) {
     final Thread result = new Thread(schedulerThreadGroup, r, namePrefix + threadNumber.getAndIncrement());
     result.setDaemon(this.deamonThread);
     result.setPriority(this.threadPriority);
     return result;
+  }
+
+  /**
+   * Creates a virtual thread with the configured properties using Java 21's Thread.Builder API.
+   *
+   * @param r the Runnable to be executed by the thread
+   * @return a new virtual thread
+   */
+  private Thread createVirtualThread(final Runnable r) {
+    String threadName = "vt-" + namePrefix + threadNumber.getAndIncrement();
+    Thread.Builder.OfVirtual builder = Thread.ofVirtual();
+    
+    if (!inheritInheritableThreadLocals) {
+      builder = builder.inheritInheritableThreadLocals(false);
+    }
+    
+    Thread thread = builder.name(threadName).unstarted(r);
+    
+    // Virtual threads are always daemon threads, but we set it explicitly for clarity
+    thread.setDaemon(true);
+    
+    // Note: Virtual threads ignore priority settings, but we don't need to warn about this
+    // as it's a documented limitation of virtual threads
+    
+    return thread;
+  }
+
+  /**
+   * Returns whether this factory creates virtual threads.
+   *
+   * @return true if this factory creates virtual threads, false otherwise
+   */
+  public boolean isUsingVirtualThreads() {
+    return useVirtualThreads;
+  }
+
+  /**
+   * Sets whether this factory should create virtual threads.
+   *
+   * @param useVirtualThreads true to create virtual threads, false to create platform threads
+   */
+  public void setUseVirtualThreads(boolean useVirtualThreads) {
+    this.useVirtualThreads = useVirtualThreads;
+  }
+
+  /**
+   * Returns whether virtual threads created by this factory inherit inheritable thread locals.
+   *
+   * @return true if virtual threads inherit inheritable thread locals, false otherwise
+   */
+  public boolean isInheritInheritableThreadLocals() {
+    return inheritInheritableThreadLocals;
+  }
+
+  /**
+   * Sets whether virtual threads created by this factory should inherit inheritable thread locals.
+   *
+   * @param inheritInheritableThreadLocals true to inherit thread locals, false otherwise
+   */
+  public void setInheritInheritableThreadLocals(boolean inheritInheritableThreadLocals) {
+    this.inheritInheritableThreadLocals = inheritInheritableThreadLocals;
   }
 }

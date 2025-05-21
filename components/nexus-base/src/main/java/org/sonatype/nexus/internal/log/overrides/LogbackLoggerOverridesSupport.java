a1ef79b9210625523ca3c844621509d9a79e4241
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-21 15:56:37 +0530

    File: components/nexus-base/src/main/java/org/sonatype/nexus/internal/log/overrides/LogbackLoggerOverridesSupport.java

--- components/nexus-base/src/main/java/org/sonatype/nexus/internal/log/overrides/LogbackLoggerOverridesSupport.java
+++ components/nexus-base/src/main/java/org/sonatype/nexus/internal/log/overrides/LogbackLoggerOverridesSupport.java
@@ -18,6 +18,8 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -54,7 +56,7 @@ public abstract class LogbackLoggerOverridesSupport
 
   protected LogbackLoggerOverridesSupport(final File logbackFile) {
     this.logbackFile = checkNotNull(logbackFile);
-    log.info("File: {}", logbackFile);
+    log.info(STR."File: \{logbackFile}");
   }
 
   protected boolean logbackFileExists() {
@@ -67,32 +69,50 @@ public abstract class LogbackLoggerOverridesSupport
   protected Map<String, LoggerLevel> readFromFile() throws Exception {
     final Map<String, LoggerLevel> result = Maps.newHashMap();
 
-    SAXParserFactory parserFactory = SafeXml.newSaxParserFactory();
-    parserFactory.setValidating(false);
-    parserFactory.setNamespaceAware(true);
-    SAXParser parser = parserFactory.newSAXParser();
-    parser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-    parser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
-    parser.parse(logbackFile, new DefaultHandler()
-    {
-      @Override
-      public void startElement(
-          final String uri,
-          final String localName,
-          final String qName,
-          final Attributes attributes) throws SAXException
-      {
-        // NOTE: ATM we are ignoring 'property' elements, this is needed for root, but is only needed
-        // NOTE: to persist as a property for use in top-level logback.xml file
+    Future<Map<String, LoggerLevel>> future = Thread.ofVirtual().name("logback-read-thread").start(() -> {
+      SAXParserFactory parserFactory = SafeXml.newSaxParserFactory();
+      parserFactory.setValidating(false);
+      parserFactory.setNamespaceAware(true);
+      parserFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+      
+      SAXParser parser = parserFactory.newSAXParser();
+      parser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+      parser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+      parser.setProperty(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+      
+      try {
+        parser.parse(logbackFile, new DefaultHandler()
+        {
+          @Override
+          public void startElement(
+              final String uri,
+              final String localName,
+              final String qName,
+              final Attributes attributes) throws SAXException
+          {
+            // NOTE: ATM we are ignoring 'property' elements, this is needed for root, but is only needed
+            // NOTE: to persist as a property for use in top-level logback.xml file
 
-        if ("logger".equals(localName)) {
-          String name = attributes.getValue("name");
-          String level = attributes.getValue("level");
-          result.put(name, LoggerLevel.valueOf(level));
-        }
+            if ("logger".equals(localName)) {
+              String name = attributes.getValue("name");
+              String level = attributes.getValue("level");
+              result.put(name, LoggerLevel.valueOf(level));
+            }
+          }
+        });
+      } catch (Exception e) {
+        log.error(STR."Error parsing logback file: \{logbackFile}", e);
+        throw e;
       }
+      return result;
     });
-    return result;
+    
+    try {
+      return future.get();
+    } catch (InterruptedException | ExecutionException e) {
+      log.error(STR."Failed to read logback overrides from file: \{logbackFile}", e);
+      throw new Exception(STR."Failed to read logback overrides: \{e.getMessage()}", e);
+    }
   }
 
   /**
@@ -101,31 +121,48 @@ public abstract class LogbackLoggerOverridesSupport
   protected void writeToFile(final Map<String, LoggerLevel> overrides) throws Exception {
     final FileReplacer fileReplacer = new FileReplacer(logbackFile);
     fileReplacer.setDeleteBackupFile(true);
-    fileReplacer.replace(output -> {
-      try (final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8))) {
-        out.write("<?xml version='1.0' encoding='UTF-8'?>");
-        out.newLine();
-        out.newLine();
-        out.write("<!--");
-        out.newLine();
-        out.write("DO NOT EDIT - Automatically generated; User-customized logging levels");
-        out.newLine();
-        out.write("-->");
-        out.newLine();
-        out.newLine();
-        out.write("<included>");
-        out.newLine();
-        for (Entry<String, LoggerLevel> entry : overrides.entrySet()) {
-          if (Logger.ROOT_LOGGER_NAME.equals(entry.getKey())) {
-            out.write(String.format("  <property name='root.level' value='%s'/>%n", entry.getValue()));
+    
+    Future<?> future = Thread.ofVirtual().name("logback-write-thread").start(() -> {
+      try {
+        fileReplacer.replace(output -> {
+          try (final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8))) {
+            out.write("<?xml version='1.0' encoding='UTF-8'?>");
+            out.newLine();
+            out.newLine();
+            out.write("<!--");
+            out.newLine();
+            out.write("DO NOT EDIT - Automatically generated; User-customized logging levels");
+            out.newLine();
+            out.write("-->");
+            out.newLine();
+            out.newLine();
+            out.write("<included>");
+            out.newLine();
+            for (Entry<String, LoggerLevel> entry : overrides.entrySet()) {
+              if (Logger.ROOT_LOGGER_NAME.equals(entry.getKey())) {
+                out.write(STR."  <property name='root.level' value='\{entry.getValue()}'/>");
+                out.newLine();
+              }
+              else {
+                out.write(STR."  <logger name='\{entry.getKey()}' level='\{entry.getValue()}'/>");
+                out.newLine();
+              }
+            }
+            out.write("</included>");
+            out.newLine();
           }
-          else {
-            out.write(String.format("  <logger name='%s' level='%s'/>%n", entry.getKey(), entry.getValue()));
-          }
-        }
-        out.write("</included>");
-        out.newLine();
+        });
+      } catch (Exception e) {
+        log.error(STR."Error writing to logback file: \{logbackFile}", e);
+        throw e;
       }
     });
+    
+    try {
+      future.get();
+    } catch (InterruptedException | ExecutionException e) {
+      log.error(STR."Failed to write logback overrides to file: \{logbackFile}", e);
+      throw new Exception(STR."Failed to write logback overrides: \{e.getMessage()}", e);
+    }
   }
 }

cf3d13b184bda5f6744b78f759e0787a6387ac5f
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-22 18:54:42 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/com/amazonaws/services/s3/S3CredentialsProviderChain.java

--- plugins/nexus-blobstore-s3/src/main/java/com/amazonaws/services/s3/S3CredentialsProviderChain.java
+++ plugins/nexus-blobstore-s3/src/main/java/com/amazonaws/services/s3/S3CredentialsProviderChain.java
@@ -14,29 +14,84 @@ package com.amazonaws.services.s3;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.core.exception.SdkClientException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A credentials provider chain for S3 that uses the default AWS SDK for Java 2.x credentials provider chain.
- * This class is located in the com.amazonaws.services.s3 package for backward compatibility
- * but uses AWS SDK for Java 2.x internally.
+ * AWS credentials provider chain for S3 that looks for credentials in the following order:
+ * <ol>
+ *   <li>Environment Variables - AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY</li>
+ *   <li>Java System Properties - aws.accessKeyId and aws.secretKey</li>
+ *   <li>Web Identity Token credentials from the environment or container</li>
+ *   <li>Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs</li>
+ *   <li>Amazon ECS container credentials - loaded from the Amazon ECS if the environment variable AWS_CONTAINER_CREDENTIALS_RELATIVE_URI is set</li>
+ *   <li>Instance profile credentials - used on EC2 instances, and delivered through the EC2 metadata service</li>
+ * </ol>
+ *
+ * This class is designed to work with AWS SDK v2.x and supports Java 21 Virtual Threads.
  *
  * @since 3.19
  */
-public class S3CredentialsProviderChain
-    implements AwsCredentialsProvider
-{
-  private final DefaultCredentialsProvider defaultCredentialsProvider;
+public class S3CredentialsProviderChain implements AwsCredentialsProvider {
+
+  private final List<AwsCredentialsProvider> credentialsProviders;
 
   /**
-   * Creates a new credentials provider chain that uses the default AWS SDK for Java 2.x credentials provider chain.
+   * Creates a new credentials provider chain with the default providers.
    */
   public S3CredentialsProviderChain() {
-    this.defaultCredentialsProvider = DefaultCredentialsProvider.create();
+    this(new ArrayList<>());
+    credentialsProviders.add(EnvironmentVariableCredentialsProvider.create());
+    credentialsProviders.add(SystemPropertyCredentialsProvider.create());
+    credentialsProviders.add(WebIdentityTokenFileCredentialsProvider.create());
+    credentialsProviders.add(ProfileCredentialsProvider.create());
+    credentialsProviders.add(ContainerCredentialsProvider.builder().build());
+    credentialsProviders.add(InstanceProfileCredentialsProvider.create());
   }
 
+  /**
+   * Creates a new credentials provider chain with the specified list of providers.
+   *
+   * @param credentialsProviders The list of credentials providers to use
+   */
+  public S3CredentialsProviderChain(List<AwsCredentialsProvider> credentialsProviders) {
+    this.credentialsProviders = credentialsProviders;
+  }
+
+  /**
+   * Returns the first set of credentials that can be loaded from the available providers.
+   *
+   * @return The first available set of credentials
+   * @throws SdkClientException If no credentials can be found
+   */
   @Override
   public AwsCredentials resolveCredentials() {
-    return defaultCredentialsProvider.resolveCredentials();
+    for (AwsCredentialsProvider provider : credentialsProviders) {
+      try {
+        return provider.resolveCredentials();
+      } catch (Exception e) {
+        // Ignore and try the next provider
+      }
+    }
+
+    throw SdkClientException.create(
+        "Unable to load AWS credentials from any provider in the chain");
+  }
+
+  /**
+   * Adds a new credentials provider to the chain.
+   *
+   * @param provider The credentials provider to add
+   */
+  public void addCredentialsProvider(AwsCredentialsProvider provider) {
+    credentialsProviders.add(provider);
   }
 }

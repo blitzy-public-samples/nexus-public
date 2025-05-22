9f6af4efa6ca8258dbbb7024eeae1ff7035889ae
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-22 15:41:32 +0530

    File: components/nexus-siesta/src/main/java/org/sonatype/nexus/siesta/internal/resteasy/NotCacheableResponseFilter.java

--- components/nexus-siesta/src/main/java/org/sonatype/nexus/siesta/internal/resteasy/NotCacheableResponseFilter.java
+++ components/nexus-siesta/src/main/java/org/sonatype/nexus/siesta/internal/resteasy/NotCacheableResponseFilter.java
@@ -14,16 +14,16 @@ package org.sonatype.nexus.siesta.internal.resteasy;
 
 import java.io.IOException;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.CacheControl;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.CacheControl;
 
 import org.sonatype.goodies.common.ComponentSupport;
 import org.sonatype.nexus.rest.NotCacheable;
 
-import static javax.ws.rs.core.HttpHeaders.CACHE_CONTROL;
-import static javax.ws.rs.core.HttpHeaders.EXPIRES;
+import static jakarta.ws.rs.core.HttpHeaders.CACHE_CONTROL;
+import static jakarta.ws.rs.core.HttpHeaders.EXPIRES;
 
 /**
  * @since 3.36

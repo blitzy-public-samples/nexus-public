eb57ed7c0594d6f1a0e3aff3dff404b7d9c167fb
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-23 01:33:05 +0530

    File: plugins/nexus-repository-apt/src/test/java/org/sonatype/nexus/repository/apt/internal/debian/Java21TestGroup.java

---
+++ plugins/nexus-repository-apt/src/test/java/org/sonatype/nexus/repository/apt/internal/debian/Java21TestGroup.java
@@ -0,0 +1,24 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.apt.internal.debian;
+
+/**
+ * Marker interface to group tests that specifically validate Java 21 features in the Debian version handling code.
+ * Used with JUnit's @Category annotation to enable selective test execution based on Java version compatibility.
+ * 
+ * This interface helps isolate tests for Java 21 specific features such as Pattern Matching for switch and
+ * Record Patterns during test runs.
+ */
+public interface Java21TestGroup
+{
+}

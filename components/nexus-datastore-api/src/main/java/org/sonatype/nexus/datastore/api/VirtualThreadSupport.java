10a50ed2a8a7a079dce698aadcbc796f79cbb3a1
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-23 02:21:05 +0530

    File: components/nexus-datastore-api/src/main/java/org/sonatype/nexus/datastore/api/VirtualThreadSupport.java

---
+++ components/nexus-datastore-api/src/main/java/org/sonatype/nexus/datastore/api/VirtualThreadSupport.java
@@ -0,0 +1,35 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.datastore.api;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation indicating that a DAO interface is optimized for execution in Java 21 Virtual Threads.
+ * <p>
+ * When applied to a DAO interface, it signals that the implementation is designed to work efficiently
+ * with Virtual Threads, avoiding thread pinning and leveraging non-blocking I/O operations where possible.
+ * This enables the DAO to be safely used in high-concurrency scenarios with Virtual Threads.
+ *
+ * @since 3.60
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface VirtualThreadSupport {
+  // Marker annotation - no additional elements
+}

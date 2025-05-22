216b8381260ffc1ea09260bd21fa3ce404cb783d
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-22 09:27:46 +0530

    File: components/nexus-features/src/main/java/org/sonatype/nexus/features/internal/Activator.java

--- components/nexus-features/src/main/java/org/sonatype/nexus/features/internal/Activator.java
+++ components/nexus-features/src/main/java/org/sonatype/nexus/features/internal/Activator.java
@@ -15,33 +15,65 @@ package org.sonatype.nexus.features.internal;
 import org.apache.karaf.features.FeaturesService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static java.lang.StringTemplate.STR;
 import static java.lang.System.getProperty;
 
 /**
  * Installs the fast {@link FeaturesService} wrapper.
+ * Compatible with Apache Karaf 4.4.4 and Java 21.
  *
  * @since 3.19
  */
 public class Activator
     implements BundleActivator
 {
+  private static final Logger log = LoggerFactory.getLogger(Activator.class);
+  
+  private static final String DISABLE_PROPERTY = "karaf.disableFastFeatures";
+  
   private FeaturesWrapper wrapper;
 
   @Override
   public void start(final BundleContext context) throws Exception {
-    // set this property to true in system.properties to turn this feature off
-    if (!"true".equalsIgnoreCase(getProperty("karaf.disableFastFeatures"))) {
+    // Check if fast features are disabled via system property
+    String disableProperty = getProperty(DISABLE_PROPERTY);
+    boolean fastFeaturesDisabled = "true".equalsIgnoreCase(disableProperty);
+    
+    if (fastFeaturesDisabled) {
+      log.info(STR."Fast features disabled by \{DISABLE_PROPERTY}=\{disableProperty}");
+      return;
+    }
+    
+    log.info(STR."Activating fast FeaturesService wrapper for Karaf 4.4.4 compatibility");
+    try {
       wrapper = new FeaturesWrapper(context);
       wrapper.open();
+      log.debug(STR."Fast FeaturesService wrapper activated successfully");
+    } 
+    catch (Exception e) {
+      log.error(STR."Failed to activate fast FeaturesService wrapper: \{e.getMessage()}", e);
+      throw e;
     }
   }
 
   @Override
   public void stop(final BundleContext context) throws Exception {
     if (wrapper != null) {
-      wrapper.close();
-      wrapper = null;
+      log.info(STR."Deactivating fast FeaturesService wrapper");
+      try {
+        wrapper.close();
+        log.debug(STR."Fast FeaturesService wrapper deactivated successfully");
+      } 
+      catch (Exception e) {
+        log.error(STR."Error deactivating fast FeaturesService wrapper: \{e.getMessage()}", e);
+        throw e;
+      }
+      finally {
+        wrapper = null;
+      }
     }
   }
 }

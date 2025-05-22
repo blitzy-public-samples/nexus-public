a6877dc2075c6735ebc5ab0d5d313b6a4aceddd5
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-22 19:31:55 +0530

    File: plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapability.java

--- plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapability.java
+++ plugins/nexus-blobstore-s3/src/main/java/org/sonatype/nexus/blobstore/s3/internal/capability/CustomS3RegionCapability.java
@@ -9,8 +9,6 @@
  * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
- *
- * This file has been updated for Java 21 compatibility.
  */
 package org.sonatype.nexus.blobstore.s3.internal.capability;
 
@@ -19,23 +17,12 @@ import javax.inject.Named;
 
 import org.sonatype.nexus.capability.CapabilitySupport;
 
-/**
- * Capability for custom S3 regions.
- * 
- * @since 3.0
- * @see CustomS3RegionCapabilityDescriptor
- * @see CustomS3RegionCapabilityConfiguration
- */
 @Named(CustomS3RegionCapabilityDescriptor.TYPE_ID)
 public class CustomS3RegionCapability
     extends CapabilitySupport<CustomS3RegionCapabilityConfiguration>
 {
   @Override
   protected CustomS3RegionCapabilityConfiguration createConfig(final Map<String, String> properties) {
-    // Using pattern matching to ensure properties is not null before creating configuration
-    if (properties instanceof Map<String, String> map) {
-      return new CustomS3RegionCapabilityConfiguration(map);
-    }
-    throw new IllegalArgumentException("Properties map cannot be null");
+    return new CustomS3RegionCapabilityConfiguration(properties);
   }
 }

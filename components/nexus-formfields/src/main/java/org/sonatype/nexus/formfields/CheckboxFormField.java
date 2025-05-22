3c2b8a9abc758d564d14e10ece55c243a93948bf
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-22 09:40:55 +0530

    File: components/nexus-formfields/src/main/java/org/sonatype/nexus/formfields/CheckboxFormField.java

--- components/nexus-formfields/src/main/java/org/sonatype/nexus/formfields/CheckboxFormField.java
+++ components/nexus-formfields/src/main/java/org/sonatype/nexus/formfields/CheckboxFormField.java
@@ -12,26 +12,79 @@
  */
 package org.sonatype.nexus.formfields;
 
+import javax.annotation.Nullable;
+
 /**
  * Checkbox field.
+ * 
+ * @since 3.0
  */
 public class CheckboxFormField
     extends AbstractFormField<Boolean>
 {
+  /**
+   * Constructor.
+   * 
+   * @param id identifier of field
+   * @param label label of field
+   * @param helpText help text of field
+   * @param required flag indicating if field is required
+   */
   public CheckboxFormField(String id, String label, String helpText, boolean required) {
     super(id, label, helpText, required);
   }
 
+  /**
+   * Constructor.
+   * 
+   * @param id identifier of field
+   */
   public CheckboxFormField(String id) {
     super(id);
   }
 
+  /**
+   * Returns the type of the form field.
+   * 
+   * @return the type identifier
+   */
+  @Override
   public String getType() {
     return "checkbox";
   }
 
+  /**
+   * Sets the initial value of the field.
+   * 
+   * @param initialValue the initial value
+   * @return this instance for method chaining
+   */
   public CheckboxFormField withInitialValue(final Boolean initialValue) {
     setInitialValue(initialValue);
     return this;
+  }
+  
+  /**
+   * Validates the given value against field constraints.
+   * 
+   * @param value the value to validate
+   * @return validation error message or null if valid
+   */
+  @Nullable
+  public String validate(final Boolean value) {
+    if (isRequired() && value == null) {
+      return STR."Field \{getId()} is required but no value was provided";
+    }
+    return null;
+  }
+  
+  /**
+   * Returns a string representation of this field for debugging purposes.
+   * 
+   * @return string representation of this field
+   */
+  @Override
+  public String toString() {
+    return STR."CheckboxFormField[id=\{getId()}, label=\{getLabel()}, required=\{isRequired()}, initialValue=\{getInitialValue()}]";
   }
 }

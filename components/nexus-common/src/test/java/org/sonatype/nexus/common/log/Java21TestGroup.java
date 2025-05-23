af89707cabf6448c38bcf4223552b5202746a349
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-23 12:00:41 +0530

    File: components/nexus-common/src/test/java/org/sonatype/nexus/common/log/Java21TestGroup.java

---
+++ components/nexus-common/src/test/java/org/sonatype/nexus/common/log/Java21TestGroup.java
@@ -0,0 +1,26 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.common.log;
+
+/**
+ * Marker interface for tests that specifically test Java 21 features.
+ * 
+ * This allows selective execution of tests that require Java 21 features
+ * such as String Templates, Virtual Threads, Record Patterns, etc.
+ *
+ * @since 3.60
+ */
+public interface Java21TestGroup
+{
+  // Marker interface
+}

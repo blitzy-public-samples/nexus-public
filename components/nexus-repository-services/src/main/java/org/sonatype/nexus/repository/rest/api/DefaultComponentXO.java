2206feabadfdedd9777429b409c3a9e3054bbc92
chaitanya-blitzy <chaitanya@blitzy.com>
2025-05-23 17:21:51 +0530

    File: components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/DefaultComponentXO.java

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/DefaultComponentXO.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/DefaultComponentXO.java
@@ -208,7 +208,7 @@ public class DefaultComponentXO
           ", repository='" + repository + '\'' +
           ", format='" + format + '\'' +
           ", assets=" + assets +
-          '}';
+          "}";
     }
     return "DefaultComponentXO{data=null}";
   }
